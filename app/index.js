bb5058e741e4f382bb17c050db35c9094b638c49
Iftekharul Islam <iislam@us.ibm.com>
2017-03-29 13:54:26 -0500

    Added fake login page for testing and demo
    Change-Id: Ife37444dcede67b54d1f257ee99322b22ac6b78c
    Signed-off-by: Iftekharul Islam <iislam@us.ibm.com>

--- app/index.js
+++ app/index.js
@@ -66,7 +66,8 @@ window.angular && (function (angular) {
            $rootScope.$on('$locationChangeSuccess', function(event){
                var path = $location.path();
                dataService.path = path;
-               if(['/','/login','/logout'].indexOf(path) == -1){
+               if(['/','/login','/logout'].indexOf(path) == -1 &&
+                path.indexOf('/login') == -1){
                    dataService.showNavigation = true;
                }else{
                    dataService.showNavigation = false;
@@ -74,6 +75,10 @@ window.angular && (function (angular) {
            });
 
            $rootScope.$on('timedout-user', function(){
+             if(sessionStorage.getItem('LOGIN_ID') == 'FAKE_ID'){
+                return;
+             }
+
              sessionStorage.removeItem('LOGIN_ID');
              $location.path('/login');
            });
