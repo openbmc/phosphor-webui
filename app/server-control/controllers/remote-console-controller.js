8b522065fa0573008e4b5388bc66d278f0ed82d1
Michael Davis <michael.s.davis@ibm.com>
2017-05-05 09:21:07 -0500

    created remote console page
    Change-Id: I72026e5ea64c9fba3089b68dfd2b8198255dabb8
    Signed-off-by: Michael Davis <michael.s.davis@ibm.com>

--- app/server-control/controllers/remote-console-controller.js
+++ app/server-control/controllers/remote-console-controller.js
@@ -19,6 +19,38 @@ window.angular && (function (angular) {
             'dataService',
             function($scope, $window, APIUtils, dataService){
                 $scope.dataService = dataService;
+
+                // See https://github.com/macton/hterm for available hterm options
+                
+                //Storage
+                hterm.defaultStorage = new lib.Storage.Local();
+
+                var term = new hterm.Terminal("foo");
+                term.onTerminalReady = function() {
+                    var io = term.io.push();
+                    io.onVTKeystroke = function(str) {
+                        console.log(str)
+                        term.io.print(str);
+                    };
+                    io.sendString = function(str) {
+                        console.log(str)
+                    };
+                };
+                term.decorate(document.querySelector('#terminal'));
+
+                //Set cursor color
+                term.prefs_.set('cursor-color', 'rgba(83, 146, 255, .5)');
+
+                //Set background color
+                term.prefs_.set('background-color', '#19273c');
+
+                //Print to console window
+                term.io.println('OpenBMC ver.00');
+                term.io.println('This is not an actual live connection.');
+                term.io.print('root@IBM:');
+                
+                //Allows keyboard input
+                term.installKeyboard();
             }
         ]
     );
