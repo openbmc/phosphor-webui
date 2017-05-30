9486f54a178726dae75d60eab305dee1ecabe796
Michael Davis <michael.s.davis@ibm.com>
2017-05-30 15:35:31 -0500

    add firmware page
    Change-Id: I3d9a2ec068e3542a848dada63a6bbebe9869bf2f
    Signed-off-by: Michael Davis <michael.s.davis@ibm.com>

--- app/configuration/controllers/firmware-controller.js
+++ app/configuration/controllers/firmware-controller.js
@@ -22,18 +22,6 @@ window.angular && (function (angular) {
                 function ($scope, $window, APIUtils, dataService, $location, $anchorScroll) {
                     $scope.dataService = dataService;
 
-                    //Check if window has scroll
-                    $scope.hasVScroll = document.body.scrollHeight > document.body.clientHeight;
-                    $scope.link = document.getElementsByClassName("btn-upload");
-                    $scope.appWindow = angular.element($window);
-
-                    //Hide/Show anchor link if window has scroll
-                    if ($scope.hasVScroll == true) {
-                        $scope.link[0].style.display = 'block';
-                    } else {
-                        $scope.link[0].style.display = 'none';
-                    }
-
                     //Scroll to target anchor
                     $scope.gotoAnchor = function () {
                         $location.hash('upload');
