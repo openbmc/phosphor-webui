d066f8258ef0824dc30ce641b2a12f1a7a3d839c
Iftekharul Islam <iffy.ryan@ibm.com>
2017-09-06 10:46:11 -0500

    Revisiting log status info in header with better styes
    Change-Id: I0b2ee399243f0bc3e5ed44bc578a0d153486ed55
    Signed-off-by: Iftekharul Islam <iffy.ryan@ibm.com>

--- app/common/services/api-utils.js
+++ app/common/services/api-utils.js
@@ -369,14 +369,18 @@ window.angular && (function (angular) {
                       var data = [];
                       var severityCode = '';
                       var priority = '';
+                      var health = '';
                       var relatedItems = [];
 
                       for(var key in content.data){
                         if(content.data.hasOwnProperty(key) && content.data[key].hasOwnProperty('Id')){
                           var severityFlags = {low: false, medium: false, high: false};
+                          var healthFlags = {critical: false, warning: false, good: false};
                           severityCode = content.data[key].Severity.split(".").pop();
                           priority = Constants.SEVERITY_TO_PRIORITY_MAP[severityCode];
                           severityFlags[priority.toLowerCase()] = true;
+                          health = Constants.SEVERITY_TO_HEALTH_MAP[severityCode];
+                          healthFlags[health.toLowerCase()] = true;
                           relatedItems = [];
                           content.data[key].associations.forEach(function(item){
                             relatedItems.push(item[2]);
@@ -388,6 +392,7 @@ window.angular && (function (angular) {
                             priority: priority,
                             severity_code: severityCode,
                             severity_flags: severityFlags,
+                            health_flags: healthFlags,
                             additional_data: content.data[key].AdditionalData.join("\n"),
                             selected: false,
                             search_text: ("#" + content.data[key].Id + " " + severityCode + " " + content.data[key].Severity + " " + content.data[key].AdditionalData.join(" ")).toLowerCase(),
@@ -424,6 +429,19 @@ window.angular && (function (angular) {
                       var title = "";
                       var tempKeyParts = [];
                       var order = 0;
+
+                      function getScaledValue(value, scale){
+                        scale = scale + "";
+                        scale = parseInt(scale, 10);
+                        var power = Math.abs(parseInt(scale,10));
+
+                        if(scale > 0){
+                          value = value * Math.pow(10, power);
+                        }else if(scale < 0){
+                          value = value / Math.pow(10, power);
+                        }
+                        return value;
+                      }
 
                       function getSensorStatus(reading){
                         var severityFlags = {critical: false, warning: false, normal: false}, severityText = '', order = 0;
