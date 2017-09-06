978ab1ca473f9c64b221fb3456b37b0c10f5eea8
Iftekharul Islam <iffy.ryan@ibm.com>
2017-09-06 10:49:48 -0500

    multi-server changes and prototype additions
    Change-Id: Icfdf19205bc4123ebd1bea086f01323ec41b4685
    Signed-off-by: Iftekharul Islam <iffy.ryan@ibm.com>

--- app/multi-server/controllers/multi-server-controller.js
+++ app/multi-server/controllers/multi-server-controller.js
@@ -13,14 +13,42 @@ window.angular && (function (angular) {
     angular
         .module('app.overview')
         .controller('multiServerController', [
-            '$scope', 
-            '$window', 
-            'APIUtils', 
+            '$scope',
+            '$window',
+            'APIUtils',
             'dataService',
             function($scope, $window, APIUtils, dataService){
                 $scope.dataService = dataService;
+                $scope.customSearch = "";
+                $scope.searchTerms = [];
                 $scope.loading = false;
+                $scope.clear = function(){
+                    $scope.customSearch = "";
+                    $scope.searchTerms = [];
+                }
 
+                $scope.doSearchOnEnter = function (event) {
+                    var search = $scope.customSearch.replace(/^\s+/g,'').replace(/\s+$/g,'');
+                    if (event.keyCode === 13 &&
+                        search.length >= 2) {
+                        $scope.searchTerms = $scope.customSearch.split(" ");
+                    }else{
+                        if(search.length == 0){
+                            $scope.searchTerms = [];
+                        }
+                    }
+                };
+
+                $scope.doSearchOnClick = function() {
+                    var search = $scope.customSearch.replace(/^\s+/g,'').replace(/\s+$/g,'');
+                    if (search.length >= 2) {
+                        $scope.searchTerms = $scope.customSearch.split(" ");
+                    }else{
+                        if(search.length == 0){
+                            $scope.searchTerms = [];
+                        }
+                    }
+                };
             }
         ]
     );
