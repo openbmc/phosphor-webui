df578a5b6abfdea7c3131e239818b57dcedaf63e
Ed Tanous <ed.tanous@intel.com>
2018-02-02 12:47:29 -0800

    Fix issue 2871
    UglifyJs aparently takes issue with one of the recent commits, which
    breaks the webui.  A quick search wasn't able to find it, so
    temporarily disable name mangling.
    
    Change-Id: I49c37c67febb3fcdd9a8d41db2d575fe757f46a6
    Fixes: https://github.com/openbmc/openbmc/issues/2871
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>

--- webpack.config.js
+++ webpack.config.js
@@ -225,7 +225,9 @@ module.exports = [
           // Reference:
           // http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
           // Minify all javascript, switch loaders to minimizing mode
-          new webpack.optimize.UglifyJsPlugin(),
+          new webpack.optimize.UglifyJsPlugin({
+            mangle: false
+          }),
 
           // Copy assets from the public folder
           // Reference: https://github.com/kevlened/copy-webpack-plugin
