87dac3fc480ac1d7e209e0a343fb6ae56e249f0e
Gunnar Mills <gmills@us.ibm.com>
2018-04-11 10:01:11 -0500

    Display DHCPEnabled
    The value of DHCPEnabled was not being displayed on the network
    page. Converted it from 0/1 to false/true.
    
    Tested: Now see the correct DHCPEnabled value displayed.
    Change-Id: Ib7c31f3804cebd5a17ac6c989e314d641573c597
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/configuration/controllers/network-controller.js
+++ app/configuration/controllers/network-controller.js
@@ -28,6 +28,7 @@ window.angular && (function (angular) {
                     $scope.interface = $scope.network.interfaces[interfaceId];
                     $scope.selectedInterface = interfaceId;
                     $scope.networkDevice = false;
+                       console.log($scope.interface.DHCPEnabled)
                 }
                 APIUtils.getNetworkInfo().then(function(data){
                     $scope.network = data.formatted_data;
@@ -35,6 +36,8 @@ window.angular && (function (angular) {
                     if($scope.network.interface_ids.length){
                        $scope.selectedInterface = $scope.network.interface_ids[0];
                        $scope.interface = $scope.network.interfaces[$scope.selectedInterface];
+                       // DHCPEnabled comes back as a 0 (false), 1 (true) convert to bool
+                       $scope.interface.DHCPEnabled = !!+$scope.interface.DHCPEnabled;
                     }
                 });
             }
