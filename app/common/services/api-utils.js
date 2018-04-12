63b99b4b4eddeac05b51bcaabfebf161505ba3e2
Gunnar Mills <gmills@us.ibm.com>
2018-04-12 13:27:20 -0500

    Fix misspelling of DomainName
    The interface object had 2 DomainName properties ("DomainName"
    and "domainName")
    
    Tested: Verified the interface object now only has 1 DomainName
    properties.
    
    Change-Id: Ic14f8e441cde8b83427b8f32f73a8bbb7efc48cc
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/common/services/api-utils.js
+++ app/common/services/api-utils.js
@@ -85,7 +85,7 @@ window.angular && (function (angular) {
                             data.interface_ids.push(interfaceId);
                             data.interfaces[interfaceId] = {
                               interfaceIname: '',
-                              domainName:'',
+                              DomainName:'',
                               MACAddress:'',
                               Nameservers: [],
                               DHCPEnabled: 0,
