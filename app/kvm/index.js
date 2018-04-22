d8cdeafa8debd0b2f485946156ba2e637e1a55c5
Ed tanous <ed@tanous.net>
2018-04-22 10:53:28 -0700

    Implement KVM in webui
    This patchset is the beginings of the infrastructure to allow KVM
    sessions through the webui.  It should not be considered complete, and
    requires a websocket capable VNC/RFB connection on the BMC.
    
    To access, open the url https://<hostname>/#kvm
    
    Change-Id: I7dda5bec41d270ae8d0913697714d4df4ec3a257
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ app/kvm/index.js
@@ -0,0 +1,25 @@
+/**
+ * A module for the kvm
+ *
+ * @module app/kvm/index
+ * @exports app/kvm/index
+ * @version 0.0.1
+ */
+
+window.angular && (function(angular) {
+  'use strict';
+
+  angular
+      .module('app.kvm', ['ngRoute', 'app.common.services'])
+      // Route configuration
+      .config([
+        '$routeProvider',
+        function($routeProvider) {
+          $routeProvider.when('/kvm', {
+            'template': require('./controllers/kvm-controller.html'),
+            'controller': 'kvmController',
+            authenticated: true
+          })
+        }
+      ]);
+})(window.angular);
