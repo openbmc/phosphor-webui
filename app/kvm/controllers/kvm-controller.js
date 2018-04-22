ccbb33cb4032fceb9223d1a053c34e748e8ce749
Ed tanous <ed@tanous.net>
2018-04-22 10:53:28 -0700

    Implement KVM in webui
    This patchset is the beginings of the infrastructure to allow KVM
    sessions through the webui.  It should not be considered complete, and
    requires a websocket capable VNC/RFB connection on the BMC.
    
    To access, open the url https://<hostname>/#kvm
    
    Change-Id: I7dda5bec41d270ae8d0913697714d4df4ec3a257
    Signed-off-by: Ed Tanous <ed.tanous@intel.com>
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ app/kvm/controllers/kvm-controller.js
@@ -0,0 +1,88 @@
+/**
+ * Controller for kvm
+ *
+ * @module app/kvm
+ * @exports kvmController
+ * @name kvmController
+ */
+
+import RFB from '@novnc/novnc/core/rfb.js';
+import {init_logging as main_init_logging} from '@novnc/novnc/core/util/logging.js';
+
+
+window.angular && (function(angular) {
+  'use strict';
+
+  angular.module('app.kvm').controller('kvmController', [
+    '$scope', '$location', '$window', '$log',
+    function($scope, $location, $window, $log) {
+      $scope.desktopName = '';
+      var rfb;
+
+      $scope.$on('$destroy', function() {
+        if (rfb) {
+          rfb.disconnect();
+        }
+      });
+
+      function updateDesktopName(rfb, name) {
+        $scope.desktopName = name;
+      };
+
+      function sendCtrlAltDel() {
+        rfb.sendCtrlAltDel();
+        return false;
+      };
+
+      function connected(e) {
+        $log.debug('RFB Connected');
+      }
+      function disconnected(e) {
+        $log.debug('RFB disconnected');
+      }
+
+      var host = $location.host();
+      var port = $location.port();
+      var target =
+          angular.element(document.querySelector('#noVNC_container'))[0];
+      main_init_logging('debug');
+      try {
+        rfb = new RFB(target, 'wss://' + host + ':' + port + '/kvmws', {});
+
+        rfb.addEventListener('connect', connected);
+        rfb.addEventListener('disconnect', disconnected);
+        rfb.addEventListener('desktopname', updateDesktopName);
+      } catch (exc) {
+        $log.error(exc);
+        updateState(
+            null, 'fatal', null, 'Unable to create RFB client -- ' + exc);
+        return;  // don't continue trying to connect
+      };
+
+      function status(text, level) {
+        var status_bar =
+            angular.element(document.querySelector('#noVNC_status_bar'))[0];
+        // Need to check if the status bar still exists.  On page change, it
+        // gets destroyed when we swap to a different view.  The system will
+        // disconnect async
+        if (status_bar) {
+          status_bar.textContent = text;
+        }
+
+        var status =
+            angular.element(document.querySelector('#noVNC_status'))[0];
+        switch (level) {
+          case 'normal':
+          case 'warn':
+          case 'error':
+            break;
+          default:
+            level = 'warn';
+        }
+        if (status) {
+          status.setAttribute('class', 'noVNC_status_' + level);
+        }
+      };
+    }
+  ]);
+})(angular);
