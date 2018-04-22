eda53ea4fc8e7d4063040e0d4c08e4d7444da105
Ed tanous <ed@tanous.net>
2018-04-22 10:53:28 -0700

    Implement KVM in webui
    This patchset is the beginings of the infrastructure to allow KVM
    sessions through the webui.  It should not be considered complete, and
    requires a websocket capable VNC/RFB connection on the BMC.
    
    To access, open the url https://<hostname>/#kvm
    
    Change-Id: I7dda5bec41d270ae8d0913697714d4df4ec3a257

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import "font-awesome/css/font-awesome.css"
+
 import angular from 'angular';
 import angular_cookies from 'angular-cookies';
 import angular_sanitize from 'angular-sanitize';
@@ -83,6 +84,10 @@ import firmware_controller from './configuration/controllers/firmware-controller
 
 import firmware_index from './firmware/index.js'
 import bmc_controller from './firmware/controllers/bmc-controller.js'
+
+import kvm_index from './kvm/index.js'
+import kvm_controller from './kvm/controllers/kvm-controller.js'
+
 import server_controller from './firmware/controllers/server-controller.js'
 
 import multi_server_index from './multi-server/index.js'
@@ -113,6 +118,7 @@ window.angular && (function (angular) {
             // Model resources
             'app.login',
             'app.overview',
+            'app.kvm',
             'app.serverControl',
             'app.serverHealth',
             'app.configuration',
