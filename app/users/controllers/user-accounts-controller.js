781521c5189b8c69e85b3a7cf46f6e1c105f3fe7
Gunnar Mills <gmills@us.ibm.com>
2018-05-17 14:45:49 -0500

    Set ignoreHttpError to false after testPassword
    Moved ignoreHttpError to the controller and set it to false after
    testPassword.
    
    Tested: Changed the password.
    Change-Id: If860c073bcedcfba6a0743ea656c76ff06fd7de0
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/users/controllers/user-accounts-controller.js
+++ app/users/controllers/user-accounts-controller.js
@@ -9,55 +9,53 @@
 window.angular && (function(angular) {
   'use strict';
 
-  angular.module('app.users').controller('userAccountsController', [
-    '$scope', '$window', 'APIUtils', 'dataService',
-    function($scope, $window, APIUtils, dataService) {
-      $scope.dataService = dataService;
-      $scope.state = 'none';
-      $scope.errorMsg = '';
+  angular
+    .module('app.users')
+    .controller('userAccountsController', [
+      '$scope',
+      '$window',
+      'APIUtils',
+      'dataService',
+      function($scope, $window, APIUtils, dataService) {
+        $scope.dataService = dataService;
+        $scope.state = 'none';
+        $scope.errorMsg = '';
 
-      $scope.changePassword = function(
-          oldPassword, newPassword, confirmNewPassword) {
-        var user = $scope.dataService.getUser();
-        if (!oldPassword || !newPassword || !confirmNewPassword) {
-          $scope.state = 'error';
-          $scope.errorMsg = 'Field is required!';
-          return false;
-        }
-        if (newPassword !== confirmNewPassword) {
-          $scope.state = 'error';
-          $scope.errorMsg = 'New passwords do not match!';
-          return false;
-        }
-        if (newPassword === oldPassword) {
-          $scope.state = 'error';
-          $scope.errorMsg = 'New password and old password match!';
-          return false;
-        }
+        $scope.changePassword = function(oldPassword, newPassword, confirmNewPassword) {
+          var user = $scope.dataService.getUser();
+          if (!oldPassword || !newPassword || !confirmNewPassword) {
+            $scope.state = 'error';
+            $scope.errorMsg = 'Field is required!';
+            return false;
+          }
+          if (newPassword !== confirmNewPassword) {
+            $scope.state = 'error';
+            $scope.errorMsg = 'New passwords do not match!';
+            return false;
+          }
+          if (newPassword === oldPassword) {
+            $scope.state = 'error';
+            $scope.errorMsg = 'New password and old password match!';
+            return false;
+          }
 
-        // Verify the oldPassword is correct
-        APIUtils.testPassword(user, oldPassword)
-            .then(
-                function(state) {
-                  APIUtils.changePassword(user, newPassword)
-                      .then(
-                          function(response) {
-                            // Clear the textboxes on a success
-                            $scope.passwordVerify = '';
-                            $scope.password = '';
-                            $scope.oldPassword = '';
-                            $scope.state = 'success';
-                          },
-                          function(error) {
-                            $scope.state = 'error';
-                            $scope.errorMsg = 'Error changing password!';
-                          });
-                },
-                function(error) {
-                  $scope.state = 'error';
-                  $scope.errorMsg = 'Old password is not correct!';
-                });
-      };
-    }
-  ]);
+          // Verify the oldPassword is correct
+          APIUtils.testPassword(user, oldPassword).then(function(state) {
+            APIUtils.changePassword(user, newPassword).then(function(response) {
+              // Clear the textboxes on a success
+              $scope.passwordVerify = '';
+              $scope.password = '';
+              $scope.oldPassword = '';
+              $scope.state = 'success';
+            }, function(error) {
+              $scope.state = 'error';
+              $scope.errorMsg = 'Error changing password!';
+            });
+          }, function(error) {
+            $scope.state = 'error';
+            $scope.errorMsg = 'Old password is not correct!';
+          });
+        };
+      }
+    ]);
 })(angular);
