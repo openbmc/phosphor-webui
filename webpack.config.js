f25ebe1b54780878223fc0002a80f8bab8cc0290
Gunnar Mills <gmills@us.ibm.com>
2018-05-22 12:45:20 -0500

    Fix issue number in TODO
    The Uglifying build error is 2871 not 2781.
    https://github.com/openbmc/openbmc/issues/2871
    
    Change-Id: I84dedc50c1cc2f45a04eacd1d83cd43bd2a7230f
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- webpack.config.js
+++ webpack.config.js
@@ -230,7 +230,7 @@ module.exports = [
           // Reference:
           // http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
           // Minify all javascript, switch loaders to minimizing mode
-          // TODO: openbmc/openbmc#2781  Mangling currently breaks the GUI.
+          // TODO: openbmc/openbmc#2871  Mangling currently breaks the GUI.
           new UglifyJsPlugin({
             uglifyOptions:{
               mangle: false
