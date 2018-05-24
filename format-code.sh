9272e423b79ae4ece1b107add8b09d19313ab73d
Andrew Geissler <geissonator@yahoo.com>
2018-05-24 12:06:58 -0700

    Add format-code.sh to run special clang verify
    The phosphor-webui repo requires only certain directories
    be verified with clang-format. This script utilizes the
    override capabilities of the format-code.sh CI process
    to handle this.
    
    Change-Id: Icb759a7d94d2b007fa88261dacc39913b1076a0a
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

---
+++ format-code.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script reformats source files using the clang-format utility.
+#
+# Files are changed in-place, so make sure you don't have anything open in an
+# editor, and you may want to commit before formatting in case of awryness.
+#
+# This must be run on a clean repository to succeed
+
+DIR=$(pwd)
+cd ${DIR}
+
+set -e
+
+echo "Formatting code under $DIR/"
+
+# Only validate certain areas of the code base for
+# formatting due to some imported code in webui
+
+if [ -f ".clang-format" ]; then
+    clang-format-5.0 -i `find ./app/ -name "*.js" -print`
+    clang-format-5.0 -i `ls *.js`
+    git --no-pager diff --exit-code
+fi
