71628450da79c9a41cf352ab25151374f4702221
Andrew Geissler <geissonator@yahoo.com>
2018-05-24 12:06:58 -0700

    Add format-code.sh to run special clang verify
    The phosphor-webui repo requires only certain directories
    be verified with clang-format. This script utilizes the
    override capabilities of the format-code.sh CI process
    to handle this.
    
    Change-Id: Icb759a7d94d2b007fa88261dacc39913b1076a0a
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- gulp_tasks/minifyvendors.js
+++ gulp_tasks/minifyvendors.js
@@ -1,42 +1,32 @@
-
 /*eslint-env node */
 /*global require: true, module: true */
 
 'use strict';
 
-var options = require('../gulp-options.js'),
-    gulp = require('gulp'),
+var options = require('../gulp-options.js'), gulp = require('gulp'),
 
     // Base dependencies
-    clean = require('gulp-clean'),
-    rename = require('gulp-rename'),
+    clean = require('gulp-clean'), rename = require('gulp-rename'),
 
     // Classical gulp dependencies
     uglify = require('gulp-uglify');
 
 var runSequence = require('run-sequence');
 
-gulp.task('minifyvendorjs:clean', function () {
+gulp.task('minifyvendorjs:clean', function() {
   return gulp.src(options.bowerFolderPath + '/**/*.min.js', {read: false})
-    .pipe(clean());
+      .pipe(clean());
 });
 
-gulp.task('minifyvendorjs:minify', function () {
-    return gulp
-        .src(options.bowerFolderPath + '/**/*.js')
-        .pipe(uglify({
-            preserveComments: 'false'
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(function(file) {
-            return file.base;
-        }))
+gulp.task('minifyvendorjs:minify', function() {
+  return gulp.src(options.bowerFolderPath + '/**/*.js')
+      .pipe(uglify({preserveComments: 'false'}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest(function(file) {
+        return file.base;
+      }));
 });
 
-module.exports = function (callback) {
-    return runSequence(
-    	'minifyvendorjs:clean',
-        'minifyvendorjs:minify',
-        callback
-    );
+module.exports = function(callback) {
+  return runSequence('minifyvendorjs:clean', 'minifyvendorjs:minify', callback);
 };
