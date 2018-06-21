52b8bde9998cb35eab9c1300d41168a56f4ed0d3
Gunnar Mills <gmills@us.ibm.com>
2018-06-21 13:16:54 -0500

    Add Power Usage Page
    Created a new page, "Manage Power Usage".
    This page is under the "Server control" tab.
    This new page currently just displays the power consumption.
    Future commits will allow the user to set the power cap from
    this page. One day, things like power consumption trending
    data could also be on this page.
    
    There was a blank unused controller which was for similar things,
    removed it.
    
    Tested: See the page and the consumption data. Uploaded screenshots
            to openbmc/openbmc#3221
    Change-Id: I9f97c58a086f34c1c1990c03d5e05b081619bd5c
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/server-health/index.js
+++ app/server-health/index.js
@@ -52,12 +52,6 @@ window.angular && (function(angular) {
                 'controller': 'sensorsController',
                 authenticated: true
               })
-              .when('/server-health/power-consumption', {
-                'template':
-                    require('./controllers/power-consumption-controller.html'),
-                'controller': 'powerConsumptionController',
-                authenticated: true
-              })
               .when('/server-health', {
                 'template': require('./controllers/log-controller.html'),
                 'controller': 'logController',
