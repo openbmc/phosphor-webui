76bbfbbc47f227eb18a65fc0c721f4f8ba5cd9d1
Gunnar Mills <gmills@us.ibm.com>
2018-07-18 14:52:02 -0500

    Add Time Owner and Time Mode
    Added the Time Owner and the Time Mode
    Created a new function that enumerates /xyz/openbmc_project/time.
    
    Tested: See Time Mode and Time Owner
    Change-Id: I3d1788bdb87b99028000d13c868b4a0cbfbc3006
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/configuration/controllers/date-time-controller.js
+++ app/configuration/controllers/date-time-controller.js
@@ -13,17 +13,27 @@ window.angular && (function(angular) {
     '$scope', '$window', 'APIUtils',
     function($scope, $window, APIUtils) {
       $scope.bmc_time = '';
+      $scope.time_mode = '';
+      $scope.time_owner = '';
       $scope.loading = true;
 
-      var getBMCTimePromise = APIUtils.getBMCTime().then(
+      var getTimePromise = APIUtils.getTime().then(
           function(data) {
-            $scope.bmc_time = data.data.Elapsed / 1000;
+            $scope.bmc_time =
+                data.data['/xyz/openbmc_project/time/bmc'].Elapsed / 1000;
+            $scope.time_owner = data.data['/xyz/openbmc_project/time/owner']
+                                    .TimeOwner.split('.')
+                                    .pop();
+            $scope.time_mode =
+                data.data['/xyz/openbmc_project/time/sync_method']
+                    .TimeSyncMethod.split('.')
+                    .pop();
           },
           function(error) {
             console.log(JSON.stringify(error));
           });
 
-      getBMCTimePromise.finally(function() {
+      getTimePromise.finally(function() {
         $scope.loading = false;
       });
     }
