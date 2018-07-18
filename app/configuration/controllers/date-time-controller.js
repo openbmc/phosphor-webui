7de38662b1bcc24a3d2c597430952ff091cd2c21
Gunnar Mills <gmills@us.ibm.com>
2018-07-18 13:01:48 -0500

    Create "Date and time settings" page
    There was a Date and time page in the GUI. Enhanced it to
    display the BMC time and added it to the menu so users
    could navigate to it.
    
    Future things planned for this page:
    Setting the time, time mode, and time owner.
    Adding a NTP server.
    
    More information can be found in the time manager README:
    https://github.com/openbmc/phosphor-time-manager/blob/master/README.md
    
    Tested: See the BMC time.
    Change-Id: Ia3ac2385cb45ec8009ed6df0899c3410fe18ef64
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/configuration/controllers/date-time-controller.js
+++ app/configuration/controllers/date-time-controller.js
@@ -10,9 +10,22 @@ window.angular && (function(angular) {
   'use strict';
 
   angular.module('app.configuration').controller('dateTimeController', [
-    '$scope', '$window', 'APIUtils', 'dataService',
-    function($scope, $window, APIUtils, dataService) {
-      $scope.dataService = dataService;
+    '$scope', '$window', 'APIUtils',
+    function($scope, $window, APIUtils) {
+      $scope.bmc_time = '';
+      $scope.loading = true;
+
+      var getBMCTimePromise = APIUtils.getBMCTime().then(
+          function(data) {
+            $scope.bmc_time = data.data.Elapsed / 1000;
+          },
+          function(error) {
+            console.log(JSON.stringify(error));
+          });
+
+      getBMCTimePromise.finally(function() {
+        $scope.loading = false;
+      });
     }
   ]);
 })(angular);
