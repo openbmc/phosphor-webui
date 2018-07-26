6773985b3f65505c0b9c9b85b1e5aa3d33ee9033
Gunnar Mills <gmills@us.ibm.com>
2018-07-26 15:26:59 -0500

    Set time mode
    Moved the selection of NTP vs Manual (time mode) to a radio
    button.
    Added code to allow the user set it.
    
    Tested: Set the time mode on a Witherspoon.
    Change-Id: If7ed889e543082300d16a67b73cc2ee55563f4e9
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- app/configuration/controllers/date-time-controller.js
+++ app/configuration/controllers/date-time-controller.js
@@ -10,12 +10,15 @@ window.angular && (function(angular) {
   'use strict';
 
   angular.module('app.configuration').controller('dateTimeController', [
-    '$scope', '$window', 'APIUtils', '$q',
-    function($scope, $window, APIUtils, $q) {
+    '$scope', '$window', 'APIUtils', '$route', '$q',
+    function($scope, $window, APIUtils, $route, $q) {
       $scope.bmc_time = '';
       $scope.time_mode = '';
       $scope.time_owner = '';
+      $scope.set_time_error = false;
+      $scope.set_time_success = false;
       $scope.loading = false;
+      var time_path = '/xyz/openbmc_project/time/';
       loadTimeData();
 
       function loadTimeData() {
@@ -23,18 +26,14 @@ window.angular && (function(angular) {
 
         var getTimePromise = APIUtils.getTime().then(
             function(data) {
-              $scope.bmc_time =
-                  data.data['/xyz/openbmc_project/time/bmc'].Elapsed / 1000;
-              $scope.host_time =
-                  data.data['/xyz/openbmc_project/time/host'].Elapsed / 1000;
+              $scope.bmc_time = data.data[time_path + 'bmc'].Elapsed / 1000;
+              $scope.host_time = data.data[time_path + 'host'].Elapsed / 1000;
 
-              $scope.time_owner = data.data['/xyz/openbmc_project/time/owner']
-                                      .TimeOwner.split('.')
-                                      .pop();
-              $scope.time_mode =
-                  data.data['/xyz/openbmc_project/time/sync_method']
-                      .TimeSyncMethod.split('.')
-                      .pop();
+              $scope.time_owner =
+                  data.data[time_path + 'owner'].TimeOwner.split('.').pop();
+              $scope.time_mode = data.data[time_path + 'sync_method']
+                                     .TimeSyncMethod.split('.')
+                                     .pop();
             },
             function(error) {
               console.log(JSON.stringify(error));
@@ -47,6 +46,37 @@ window.angular && (function(angular) {
         $q.all(promises).finally(function() {
           $scope.loading = false;
         });
+      }
+      $scope.setTime = function() {
+        $scope.set_time_error = false;
+        $scope.set_time_success = false;
+        $scope.loading = true;
+        var promises = [
+          setTimeMode(),
+        ];
+
+        $q.all(promises).finally(function() {
+          $scope.loading = false;
+          if (!$scope.set_time_error) {
+            $scope.set_time_success = true;
+          }
+        });
+      };
+      $scope.refresh = function() {
+        $route.reload();
+      };
+
+      function setTimeMode() {
+        return APIUtils
+            .setTimeMode(
+                'xyz.openbmc_project.Time.Synchronization.Method.' +
+                $scope.time_mode)
+            .then(
+                function(data) {},
+                function(error) {
+                  $scope.set_time_error = true;
+                  console.log(JSON.stringify(error));
+                });
       }
     }
   ]);
