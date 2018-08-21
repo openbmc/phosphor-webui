b67f4e891f8a130d924d748d79c01970ef92b075
beccabroek <beccabroek@gmail.com>
2018-08-21 13:10:14 -0500

    Serial LAN console export
    Added ability to export console output to .txt file.
    Also ability to open serial lan console in new window.
    
    Tested: Powered on server in order to test that output
        was displaying and exported to txt file.
    
    Change-Id: Ie73ba2dc61f7d09d6ca98e2d162364c8119d2e46
    Signed-off-by: beccabroek <beccabroek@gmail.com>

--- app/server-control/controllers/remote-console-window-controller.js
+++ app/server-control/controllers/remote-console-window-controller.js
@@ -6,53 +6,78 @@
  * @name remoteConsoleController
  */
 
+import {hterm, lib} from 'hterm-umdjs';
+
 window.angular && (function(angular) {
   'use strict';
 
-  angular.module('app.serverControl')
-      .controller('remoteConsoleWindowController', [
-        '$scope', '$window', 'APIUtils', 'dataService',
-        function($scope, $window, APIUtils, dataService) {
-          $scope.dataService = dataService;
-          dataService.showNavigation = false;
+  angular.module('app.serverControl').controller('remoteConsoleWindowController', [
+    '$scope', '$window', 'dataService',
+    function($scope, $window, dataService) {
+      $scope.dataService = dataService;
+      dataService.showNavigation = false;
+      dataService.bodyStyle = {'background': 'white'};
 
-          // See https://github.com/macton/hterm for available hterm options
+      // See https://github.com/macton/hterm for available hterm options
 
-          // Storage
-          hterm.defaultStorage = new lib.Storage.Local();
+      hterm.defaultStorage = new lib.Storage.Local();
+      var term = new hterm.Terminal('host-console');
+      term.decorate(document.querySelector('#terminal'));
+      // Set cursor color
+      term.prefs_.set('cursor-color', 'rgba(83, 146, 255, .5)');
+      // Set background color
+      term.prefs_.set('background-color', '#19273c');
+      // Allows keyboard input
+      term.installKeyboard();
+      $scope.export_text = '';
 
-          var term = new hterm.Terminal('foo');
-          term.onTerminalReady = function() {
-            var io = term.io.push();
-            io.onVTKeystroke = function(str) {
-              console.log(str);
-              term.io.print(str);
-            };
-            io.sendString = function(str) {
-              console.log(str);
-            };
-          };
-          term.decorate(document.querySelector('#terminal'));
+      // The BMC exposes a websocket at /console0. This can be read
+      // or written to access the host serial console.
+      var hostname = dataService.getHost().replace('https://', '');
+      var host = 'wss://' + hostname + '/console0';
+      var ws = new WebSocket(host);
+      ws.onmessage = function(evt) {
+        // websocket -> terminal
+        term.io.print(evt.data);
+      };
 
-          // Set cursor color
-          term.prefs_.set('cursor-color', 'rgba(83, 146, 255, .5)');
+      // terminal -> websocket
+      term.onTerminalReady = function() {
+        var io = term.io.push();
+        io.onVTKeystroke = function(str) {
+          ws.send(str);
+        };
+        io.sendString = function(str) {
+          ws.send(str);
+        };
+      };
 
-          // Set background color
-          term.prefs_.set('background-color', '#19273c');
-
-          // Print to console window
-          term.io.println('OpenBMC ver.00');
-          term.io.println('This is not an actual live connection.');
-          term.io.print('root@IBM:');
-
-          // Allows keyboard input
-          term.installKeyboard();
-
-          $scope.close = function() {
-            dataService.setRemoteWindowInactive();
-            $window.close();
-          };
+      ws.onopen = function() {
+        console.log('websocket opened');
+      };
+      ws.onclose = function() {
+        console.log('websocket closed');
+      };
+      $scope.$on('$destroy', function() {
+        if (ws) {
+          ws.close();
         }
-      ]);
-
+      });
+      $scope.close = function() {
+        $window.close();
+      };
+      $scope.getExportDate = function() {
+        return new Date();
+      };
+      $scope.determineExportData = function() {
+        $scope.export_text = term.getRowsText(0, term.getRowCount());
+      };
+      $scope.openTerminalWindow = function() {
+        dataService.setRemoteWindowActive();
+        $window.open(
+            '#/server-control/remote-console-window', 'Remote Console Window',
+            'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=600,height=550');
+      };
+    }
+  ]);
 })(angular);
