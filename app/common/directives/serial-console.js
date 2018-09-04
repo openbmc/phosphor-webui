7ee0f4349453fd60a433ead589b82262b0d89cfa
beccabroek <beccabroek@gmail.com>
2018-09-04 09:38:13 -0500

    SOL console in new window button
    Created button to open SOL console in new
    window.
    
    Resolves openbmc/openbmc#3153
    
    Tested: Verified that button brings up new window
        and  that SOL console functions in new window.
    
    Change-Id: Idf8ff314e9a14cba0820823afd1ce60e4b3bbc4f
    Signed-off-by: beccabroek <beccabroek@gmail.com>

--- app/common/directives/serial-console.js
+++ app/common/directives/serial-console.js
@@ -8,7 +8,7 @@ window.angular && (function(angular) {
       return {
         'restrict': 'E',
         'template': require('./serial-console.html'),
-        'scope': {'path': '='},
+        'scope': {'path': '=', 'showTabBtn': '=?'},
         'controller': [
           '$scope', '$window', 'dataService',
           function($scope, $window, dataService) {
@@ -52,6 +52,13 @@ window.angular && (function(angular) {
             };
             ws.onclose = function() {
               console.log('websocket closed');
+            };
+            $scope.openTerminalWindow = function() {
+              dataService.setRemoteWindowActive();
+              $window.open(
+                  '#/server-control/remote-console-window',
+                  'Remote Console Window',
+                  'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=600,height=550');
             };
             $scope.$on('$destroy', function() {
               if (ws) {
