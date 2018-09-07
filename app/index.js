f7a7d09c2139d3af2853bccee79578422dc2984a
James Feist <james.feist@linux.intel.com>
2018-09-07 14:25:48 -0700

    Add Redfish into the web-ui
    Currently only get is supported, but to add patch
    in the future redfish support should be moved from
    being hosted directly by bmcweb to the webui.
    
    Tested-by:
    Navigated to http://localhost:8080/#/redfish/v1/ with
    bmc attached and was able to view all of redfish.
    
    Change-Id: I1dc3936b6b48835a1f69698fcb4fd6fcdb6d91ac
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- app/index.js
+++ app/index.js
@@ -70,6 +70,8 @@ import inventory_overview_controller from './server-health/controllers/inventory
 import log_controller from './server-health/controllers/log-controller.js';
 import sensors_overview_controller from './server-health/controllers/sensors-overview-controller.js';
 
+import redfish_index from './redfish/index.js';
+import redfish_controller from './redfish/controllers/redfish-controller.js';
 import configuration_index from './configuration/index.js';
 import date_time_controller from './configuration/controllers/date-time-controller.js';
 import network_controller from './configuration/controllers/network-controller.js';
@@ -100,7 +102,7 @@ window.angular && (function(angular) {
             // Model resources
             'app.login', 'app.overview', 'app.serverControl',
             'app.serverHealth', 'app.configuration', 'app.users',
-            'app.multiServer'
+            'app.redfish', 'app.multiServer'
           ])
       // Route configuration
       .config([
