12a4b63d7cfa76e0849bdb05970393764d92dfcf
AppaRao Puli <apparao.puli@linux.intel.com>
2018-10-12 14:44:04 +0530

    Invoke Redfish uri from WebUI for password change
    This commit changes the password change uri from
    rest dbus call to standard redfish uri in WebUI.
    
    Tested:
    Updated the password from WebUI and checked user login
    from WebUI with old (Failure) and new password(success).
    
    Change-Id: I98ecc7f419ddcfdbfac9c212ad2834479e9986b5
    Signed-off-by: AppaRao Puli <apparao.puli@linux.intel.com>

--- app/common/services/api-utils.js
+++ app/common/services/api-utils.js
@@ -463,25 +463,15 @@ window.angular && (function(angular) {
                   });
         },
         changePassword: function(user, newPassword) {
-          var deferred = $q.defer();
           $http({
-            method: 'POST',
-            url: DataService.getHost() + '/xyz/openbmc_project/user/' + user +
-                '/action/SetPassword',
+            method: 'PATCH',
+            url: DataService.getHost() + '/redfish/v1/AccountService/Accounts/' + user,
             withCredentials: true,
-            data: JSON.stringify({'data': [newPassword]}),
-            responseType: 'arraybuffer'
+            data: JSON.stringify({'Password': newPassword})
           })
-              .then(
-                  function(response, status, headers) {
-                    deferred.resolve(
-                        {data: response, status: status, headers: headers});
-                  },
-                  function(error) {
-                    console.log(error);
-                    deferred.reject(error);
-                  });
-          return deferred.promise;
+              .then(function(response) {
+                    return response;
+              });
         },
         chassisPowerOff: function() {
           var deferred = $q.defer();
