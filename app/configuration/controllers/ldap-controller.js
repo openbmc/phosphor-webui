9860c5a69db93de2be461f68bd83d1e8def0a812
beccabroek <beccabroek@gmail.com>
2018-10-23 16:07:42 -0500

    LDAP settings page
    Creates LDAP settings page and allows
    user to create and delete LDAP role
    groups.
    
    Tested:  Able to add and remove LDAP
    role groups on a witherspoon
    
    Change-Id: I3ae34760e26ec559bc8f7aaa0d23a1253f34f46f
    Signed-off-by: beccabroek <beccabroek@gmail.com>

---
+++ app/configuration/controllers/ldap-controller.js
@@ -0,0 +1,121 @@
+/**
+ * Controller for LDAP
+ *
+ * @module app/configuration
+ * @exports ldapController
+ * @name ldapController
+ */
+
+window.angular && (function(angular) {
+  'use strict';
+
+  angular.module('app.configuration').controller('ldapController', [
+    '$scope', 'APIUtils', '$q',
+    function($scope, APIUtils, $q) {
+      $scope.groups = [];
+      $scope.selectedGroups = [];
+      $scope.ldapConfig = {};
+      $scope.newGroup = {};
+      $scope.activeModal = '';
+      $scope.ldapDisabled = true;
+      $scope.loading = true;
+      $scope.error = false;
+      $scope.success = false;
+      $scope.userMessage = '';
+
+      $scope.loadLdapInfo = function() {
+        $scope.loading = true;
+        var getLdapConfiguration = APIUtils.getLdapConfiguration().then(
+            function(data) {
+              // If LDAP config exists, LDAP is enabled.
+              if (data.config) {
+                $scope.groups = data.groups;
+                $scope.ldapDisabled = false;
+              }
+            },
+            function(error) {
+              console.log(JSON.stringify(error));
+            });
+        var getUserPrivileges = APIUtils.getUserPrivileges().then(
+            function(data) {
+              if (data) {
+                $scope.userPrivileges = data;
+              }
+            },
+            function(error) {
+              console.log(JSON.stringify(error));
+            });
+        var promises = [getLdapConfiguration, getUserPrivileges];
+        $q.all(promises).finally(function() {
+          $scope.loading = false;
+        });
+      };
+
+      $scope.$watch('groups', function() {
+        $scope.selectedGroups = $scope.groups.filter(function(group) {
+          return group.isSelected === true;
+        });
+      }, true);
+
+      $scope.saveGroupSettings = function() {
+        $scope.loading = true;
+        if ($scope.activeModal === 'add') {
+          if (!$scope.newGroup.name || !$scope.newGroup.privilege) {
+            // TODO: Highlight blank fields
+            $scope.error = true;
+            $scope.userMessage =
+                'Error adding Role Group. All fields are required.';
+            $scope.loading = false;
+            $scope.activeModal = '';
+            return;
+          }
+          for (var i = 0; i < $scope.groups.length; i++) {
+            if ($scope.groups[i].groupName === $scope.newGroup.name) {
+              $scope.error = true;
+              $scope.userMessage =
+                  'Error adding Role Group. Role Group with the name ' +
+                  $scope.newGroup.name + ' already exists';
+              $scope.loading = false;
+              $scope.activeModal = '';
+              return;
+            }
+          }
+          APIUtils
+              .createLdapGroup($scope.newGroup.name, $scope.newGroup.privilege)
+              .then(
+                  function(data) {
+                    $scope.loadLdapInfo();
+                  },
+                  function(error) {
+                    $scope.error = true;
+                    $scope.userMessage =
+                        'Error adding Role Group ' + $scope.newGroup.name;
+                    console.log(JSON.stringify(error));
+                  })
+              .finally(function() {
+                $scope.newGroup = {};
+                $scope.loading = false;
+                $scope.activeModal = ''
+              });
+        } else if ($scope.activeModal === 'delete') {
+          APIUtils.deleteLdapGroups($scope.selectedGroups)
+              .then(
+                  function(data) {
+                    $scope.loadLdapInfo();
+                  },
+                  function(error) {
+                    $scope.error = true;
+                    $scope.userMessage = 'Error removing Role Group(s)';
+                    console.log(JSON.stringify(error));
+                  })
+              .finally(function() {
+                $scope.loading = false;
+                $scope.activeModal = ''
+              });
+        }
+        // TODO: Modify Role Groups with PUT
+      };
+      $scope.loadLdapInfo();
+    }
+  ]);
+})(angular);
