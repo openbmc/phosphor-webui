d829eaf8652cf3166454dd82604090fbf91a7fe0
beccabroek <beccabroek@gmail.com>
2018-10-25 11:37:58 -0500

    Add and delete LDAP config
    Creates panel on LDAP settings page for
    LDAP configuration. User can delete config
    by disabling LDAP in the UI and can create
    config by enabling and filling out the fields.
    
    Tested: Able to add and delete LDAP config
      on witherspoon. Able to login with LDAP
      credentials when config is added and unable
      to login with credentials when LDAP is
      disabled from the UI.
    
    Change-Id: I4267aa3b04d8256267c31460305af7488017c871
    Signed-off-by: beccabroek <beccabroek@gmail.com>

--- app/common/services/api-utils.js
+++ app/common/services/api-utils.js
@@ -136,6 +136,26 @@ window.angular && (function(angular) {
                 return content.data['AllPrivileges'];
               });
         },
+        createLdapConfig: function(config) {
+          return $http({
+                   method: 'POST',
+                   url: DataService.getHost() +
+                       '/xyz/openbmc_project/user/ldap/action/CreateConfig',
+                   withCredentials: true,
+                   data: JSON.stringify({
+                     'data': [
+                       config.secureLdap, config.serverUri, config.bindDn,
+                       config.baseDn, config.bindDnPassword,
+                       'xyz.openbmc_project.User.Ldap.Create.SearchScope.' +
+                           config.searchScope,
+                       Constants.LDAP_TYPE[config.ldapType]
+                     ]
+                   })
+                 })
+              .then(function(response) {
+                return response.data;
+              });
+        },
         createLdapGroup: function(groupName, groupPrivilege) {
           return $http({
                    method: 'POST',
