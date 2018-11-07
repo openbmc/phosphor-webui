5700f7862e5b7b43085c93dc8e30cea845a83015
beccabroek <beccabroek@gmail.com>
2018-11-07 12:22:31 -0600

    Create certificate management page
    Displays certificates and the ability to add up to one
    of each type of certificate (as limited by the backend
    implementation). HTTPS certificate and LDAP client cert
    are implemented in this commit, with the ability to add
    more types as needed by adding them to the constants.js
    CERTIFICATE_TYPES array.
    
    Also provides the ability to replace a certificate once
    it is added.
    
    Resolves openbmc/phosphor-webui#43
    
    Tested: loaded onto a witherspoon and able to view and
            replace both the HTTPS certificate and the
            LDAP certificate. GUI only allows to upload an
            LDAP certificate if one doesn't already exist.
            The GUI limits the user to one file per type as
            expected at this time and provides the
            appropriate validation messages. Alert messages
            appear above the table if the certificate is
            expired or within 30 days of expiring.
    
    Change-Id: I345267280ecd3cb257e9304886cde9ebb69b1240
    Signed-off-by: beccabroek <beccabroek@gmail.com>
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>

---
+++ app/common/directives/certificate.js
@@ -0,0 +1,55 @@
+window.angular && (function(angular) {
+  'use strict';
+
+  angular.module('app.common.directives').directive('certificate', [
+    'APIUtils',
+    function(APIUtils) {
+      return {
+        'restrict': 'E',
+        'template': require('./certificate.html'),
+        'scope': {'cert': '=', 'reload': '&'},
+        'controller': [
+          '$scope', 'APIUtils', 'toastService',
+          function($scope, APIUtils, toastService) {
+            var certificateType = 'PEM';
+            $scope.loading = true;
+            $scope.replaceCertificate = function(certificate) {
+              if (certificate.file.name.split('.').pop() !==
+                  certificateType.toLowerCase()) {
+                toastService.error(
+                    'Certificate must be replaced with a .pem file.');
+                return;
+              }
+              var file =
+                  document
+                      .getElementById(
+                          'upload_' + certificate.Description + certificate.Id)
+                      .files[0];
+              var reader = new FileReader();
+              reader.onloadend = function(e) {
+                var data = {};
+                data.CertificateString = e.target.result;
+                data.CertificateUri = {'@odata.id': certificate['@odata.id']};
+                data.CertificateType = certificateType;
+                APIUtils.replaceCertificate(data).then(
+                    function(data) {
+                      $scope.loading = false;
+                      toastService.success(
+                          certificate.Description + ' was replaced.');
+                      $scope.reload();
+                    },
+                    function(error) {
+                      console.log(error);
+                      $scope.loading = false;
+                      toastService.error(
+                          'Unable to replace ' + certificate.Description);
+                    });
+              };
+              reader.readAsBinaryString(file);
+            };
+          }
+        ]
+      };
+    }
+  ]);
+})(window.angular);
