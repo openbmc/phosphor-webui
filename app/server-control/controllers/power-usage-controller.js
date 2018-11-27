e1afb8fe5338f0ac879a0058cb4b8506fbbfab04
Steven <bao_gang_168@163.com>
2018-11-27 16:24:29 +0800

    add slot

--- app/server-control/controllers/power-usage-controller.js
+++ app/server-control/controllers/power-usage-controller.js
@@ -8,12 +8,13 @@ window.angular && (function(angular) {
       $scope.dataService = dataService;
       $scope.loading = false;
 	  $scope.ssdInfo = [];
+	  $scope.slotInfo = [];
 	  	    
 	    function showSSDData(ssdData) {
-			console.log("44444444");
-			console.log(ssdData);
+			//console.log("44444444");
+			//console.log(ssdData);
 			for(var num = 0; num < ssdData.length; num++) {
-				console.log("555555");
+				//console.log("555555");
 				var ssdAddr = ssdData[num].value >>> 27 & 0x1f;
 				var ssdType = ssdData[num].value >>> 24 & 0x07;
 				var linkSpeed = ssdData[num].value >>> 20 & 0x0f;
@@ -22,18 +23,8 @@ window.angular && (function(angular) {
 				var linkWidth = ssdData[num].value >>> 8 & 0x0f;
 				var parId = ssdData[num].value >>> 2 & 0x0f;
 				var Inserted = ssdData[num].value >>> 1 & 0x01;
-				var linkStatus = ssdData[num].value & 0x01;
-				console.log(ssdAddr);
-				console.log(ssdType);
-				console.log(linkSpeed);
-				console.log(state);
-				console.log(cfgWidth);
-				console.log(linkWidth);
-				console.log(parId);
-				console.log(Inserted);
-				console.log(linkStatus);
-				console.log(ssdData[num].title);
-				
+				var linkStatus = ssdData[num].value & 0x01;				
+				//console.log(ssdData[num].title);				
 				$scope.ssdInfo.push(Object.assign(
 				{
 					ssd_addr: ssdAddr,
@@ -49,30 +40,65 @@ window.angular && (function(angular) {
 				{title: ssdData[num].title}));
 			}
 			
-	    };	  
+	    };	 
+			
+		function showSlotData(slotData) {
+			for(var num = 0; num < slotData.length; num++) {
+				var present slotData[num].value >>> 31 & 0x01;
+				var slotAddr = slotData[num].value >>> 27 & 0x0f;
+				var cableType = slotData[num].value >>> 24 & 0x07;
+				var linkActive = slotData[num].value >>> 23 & 0x01;
+				var linkWidth = slotData[num].value >>> 19 & 0x0f;
+				var linkStatus = slotData[num].value >>> 16 & 0x07;
+				var parId = slotData[num].value >>> 8 & 0x0f;
+				var state = slotData[num].value >>> 4 & 0x0f;
+				var uspOrDsp = slotData[num].value & 0x0f;
+				
+				$scope.slotInfo.push(Object.assign(
+				{
+					present: present,
+					slot_addr: slotAddr,
+					cable_type: cableType,
+					link_active: linkActive,
+					link_wd: linkWidth,
+					link_st: linkStatus,
+					par_id: parId,
+					status: state,
+					usp_or_dsp: uspOrDsp,
+				},
+				{title: slotData[num].title}));
+			}
+			
+		};
 		
 	  $scope.getSensorData = function() {
 		var ssdData = [];
+		var slotData = [];
 		$scope.loading = true;		
         APIUtils.getAllSensorStatus(function(data, originalData) {
-			console.log("11111111111");
-			console.log(data);
+			//console.log("11111111111");
+			//console.log(data);
 			//$scope.data = data;
 			for(var j = 0; j < 24; j++) {
-				var flag = "ssd"+(j + 1);				
+				var ssd = "ssd"+(j + 1);
+				var slot = "slot"+(j + 1);
 				for(var i = 0; i < data.length; i++) {
-					console.log("2222222");
-					console.log(data);
-					console.log(flag);
-					if(data[i].search_text.indexOf(flag) != -1) { 
-					    console.log("3333333");
-						ssdData[j] = data[i];
+					//console.log("2222222");
+					//console.log(data);
+					//console.log(ssd);
+					if(data[i].search_text.indexOf(ssd) != -1) { 
+					    //console.log("3333333");
 						//ssdData.push(data[i]);
-						break;
+						ssdData[j] = data[i];
+						//break;
+					}
+					if(data[i].search_text.indexOf(slot) != -1) {
+						slotData[j] = data[i];
 					}
 				}	
 			}
-			showSSDData(ssdData);			
+			showSSDData(ssdData);	
+			showSlotData			
             $scope.loading = false;
         });
       };
