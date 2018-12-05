794dcd8d7a1df818719e42708865c6a54530a26b
beccabroek <beccabroek@gmail.com>
2018-12-05 17:00:37 -0600

    Modify role group privileges
    Adds ability to modify privileges for an LDAP role
    group.
    
    Change-Id: Id2216a40b3dfd28e9b1bddae5e514ef0149e9133
    Signed-off-by: beccabroek <beccabroek@gmail.com>

--- app/configuration/controllers/ldap-controller.js
+++ app/configuration/controllers/ldap-controller.js
@@ -56,6 +56,10 @@ window.angular && (function(angular) {
         });
       }, true);
 
+      $scope.getSelectedGroup = function() {
+        return $scope.selectedGroups[0];
+      };
+
       $scope.saveGroupSettings = function() {
         $scope.loading = true;
         $scope.error = false;
@@ -104,8 +108,25 @@ window.angular && (function(angular) {
                 $scope.loading = false;
                 $scope.activeModal = ''
               });
+        } else if ($scope.activeModal === 'modify') {
+          var selectedGroup = $scope.getSelectedGroup();
+          APIUtils
+              .modifyLdapGroupPrivilege(
+                  selectedGroup.id, selectedGroup.privilege)
+              .then(
+                  function(data) {
+                    $scope.loadLdapInfo();
+                  },
+                  function(error) {
+                    $scope.error = true;
+                    $scope.userMessage = 'Error modifying Role Group';
+                    console.log(JSON.stringify(error));
+                  })
+              .finally(function() {
+                $scope.loading = false;
+                $scope.activeModal = ''
+              });
         }
-        // TODO: Modify Role Groups with PUT
       };
       $scope.loadLdapInfo();
     }
