8112b6add6c03f01b73505287c2f379907b58542
Cheng C Yang <cheng.c.yang@linux.intel.com>
2019-02-14 15:24:28 +0800

    Set SOL Screen dynamically
    To make EWS SOL Screen size match the real BIOS console, need to
    set the screen dynamically. Web page get terminal resolution from
    config.json and calculate frame width and height base on the
    resolution and fontsize. Web page use this width and height to
    change the screen size. If no related configuration in config.json
    the SOL Screen will keep the original size before this code change.
    
    Tested By:
    Add "customConsoleDisplaySize": {"width": 100, "height": 32} to
    config.json, and open EWS Server Serial over LAN console page, the
    size of console client can match 100*32 characters. Set console
    redirection to 100*32 in BIOS setup page, the page show correctly
    in this SOL client in EWS. After enter OS, the linux console can
    show correctly in SOL client.
    After remove "customConsoleDisplaySize" in config.json, the size
    of console client in EWS show the same as the code change in this
    patch.
    
    Change-Id: I09bfe4e67db1387abcd57ee3a13b912957cf95b0
    Signed-off-by: Yang Cheng <cheng.c.yang@linux.intel.com>

--- app/common/directives/serial-console.js
+++ app/common/directives/serial-console.js
@@ -2,7 +2,28 @@ import {Terminal} from 'xterm';
 import style from 'xterm/dist/xterm.css';
 import * as attach from 'xterm/lib/addons/attach/attach';
 import * as fit from 'xterm/lib/addons/fit/fit';
+var configJSON = require('../../../config.json');
 
+function measureChar(term) {
+  var span = document.createElement('span');
+  var fontFamily = 'courier-new';
+  var fontSize = 15;
+  var rect;
+
+  span.textContent = 'W';
+  try {
+    fontFamily = term.getOption('fontFamily');
+    fontSize = term.getOption('fontSize');
+  } catch (err) {
+    console.log('get option failure');
+  }
+  span.style.fontFamily = fontFamily;
+  span.style.fontSize = fontSize + 'px';
+  document.body.appendChild(span);
+  rect = span.getBoundingClientRect();
+  document.body.removeChild(span);
+  return rect;
+}
 
 window.angular && (function(angular) {
   'use strict';
@@ -24,8 +45,30 @@ window.angular && (function(angular) {
             Terminal.applyAddon(attach);  // Apply the `attach` addon
             Terminal.applyAddon(fit);     // Apply the `fit` addon
 
+            var border = 10;
             var term = new Terminal();
-            term.open(document.getElementById('terminal'));
+            var terminal = document.getElementById('terminal');
+            var customConsole;
+            var charSize;
+            var termContainer;
+
+            term.open(terminal);
+            customConsole = configJSON.customConsoleDisplaySize;
+
+            if (customConsole != null) {
+              charSize = measureChar(term);
+              termContainer = document.getElementById('term-container');
+              if (termContainer != null) {
+                if (customConsole.width) {
+                  termContainer.style.width =
+                      (charSize.width * customConsole.width + border) + 'px';
+                }
+                if (customConsole.height) {
+                  terminal.style.height =
+                      (charSize.height * customConsole.height + border) + 'px';
+                }
+              }
+            }
             term.fit();
             var SOL_THEME = {
               background: '#19273c',
