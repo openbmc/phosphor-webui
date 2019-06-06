f55c51284e2ef915451bfc06526374f563789683
Derick Montague <derick.montague@ibm.com>
2019-06-06 18:51:36 -0500

    Add ESLint ane Prettier
    This will add ESLinting for JavaScript and Prettier
    for SCSS. clang-format will still format the code.
    
    - Set up select group of angular and eslinting rules
    - Added .prettierignore to keep Prettier from formatting
      JavaScript
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: If7a7a63754a35a35e85daa748a499627cec59d7e

--- app/configuration/controllers/snmp-controller.js
+++ app/configuration/controllers/snmp-controller.js
@@ -6,17 +6,23 @@
  * @name snmpController
  */
 
-window.angular && (function(angular) {
-  'use strict';
+window.angular &&
+  (function(angular) {
+    'use strict';
 
-  angular.module('app.configuration').controller('snmpController', [
-    '$scope', '$window', 'APIUtils', '$route', '$q', 'toastService',
-    function($scope, $window, APIUtils, $route, $q, toastService) {
-      $scope.managers = [];
-      $scope.loading = true;
-      $scope.managersToDelete = [];
+    angular.module('app.configuration').controller('snmpController', [
+      '$scope',
+      '$window',
+      'APIUtils',
+      '$route',
+      '$q',
+      'toastService',
+      function($scope, $window, APIUtils, $route, $q, toastService) {
+        $scope.managers = [];
+        $scope.loading = true;
+        $scope.managersToDelete = [];
 
-      var getSNMPManagers = APIUtils.getSNMPManagers().then(
+        var getSNMPManagers = APIUtils.getSNMPManagers().then(
           function(data) {
             // Convert to array of objects from an object of objects, easier
             // to manipulate (e.g. add/remove). Convert key to a path property.
@@ -27,104 +33,115 @@ window.angular && (function(angular) {
                 updatePort: false,
                 address: data.data[key].Address,
                 updateAddress: false
-              })
+              });
             }
           },
           function(error) {
             toastService.error('Unable to load SNMP settings.');
             console.log(JSON.stringify(error));
-          });
-
-      getSNMPManagers.finally(function() {
-        $scope.loading = false;
-      });
-
-      $scope.addNewSNMPManager = function() {
-        $scope.managers.push({address: '', port: ''});
-      };
-
-      $scope.removeSNMPManager = function(index) {
-        // If the SNMP Manager has a path it exists on the backend and we
-        // need to make a call to remove it
-        if ($scope.managers[index].path) {
-          $scope.managersToDelete.push($scope.managers[index].path);
-        }
-        $scope.managers.splice(index, 1);
-      };
-
-      $scope.refresh = function() {
-        $route.reload();
-      };
-
-      $scope.setSNMP = function() {
-        $scope.loading = true;
-        var promises = [];
-
-        // Validate that no field are empty and port is valid. Port value is
-        // undefined if it is an invalid number.
-        for (var i in $scope.managers) {
-          if (!$scope.managers[i].address || !$scope.managers[i].port) {
-            $scope.loading = false;
-            toastService.error('Cannot save. Please resolve errors on page.');
-            return;
           }
-        }
-        // Iterate in reverse so can splice
-        // https://stackoverflow.com/questions/9882284/looping-through-array-and-removing-items-without-breaking-for-loop
-        var i = $scope.managers.length;
-        while (i--) {
-          // If the manager does not have a 'path', it is a new manager
-          // and needs to be created
-          if (!$scope.managers[i].path) {
-            promises.push(addManager(
-                $scope.managers[i].address, $scope.managers[i].port));
-          } else {
-            if ($scope.managers[i].updateAddress) {
-              promises.push(setManagerAddress(
-                  $scope.managers[i].path, $scope.managers[i].address));
-            }
-            if ($scope.managers[i].updatePort) {
-              promises.push(setManagerPort(
-                  $scope.managers[i].path, $scope.managers[i].port));
+        );
+
+        getSNMPManagers.finally(function() {
+          $scope.loading = false;
+        });
+
+        $scope.addNewSNMPManager = function() {
+          $scope.managers.push({ address: '', port: '' });
+        };
+
+        $scope.removeSNMPManager = function(index) {
+          // If the SNMP Manager has a path it exists on the backend and we
+          // need to make a call to remove it
+          if ($scope.managers[index].path) {
+            $scope.managersToDelete.push($scope.managers[index].path);
+          }
+          $scope.managers.splice(index, 1);
+        };
+
+        $scope.refresh = function() {
+          $route.reload();
+        };
+
+        $scope.setSNMP = function() {
+          $scope.loading = true;
+          var promises = [];
+
+          // Validate that no field are empty and port is valid. Port value is
+          // undefined if it is an invalid number.
+          for (let i in $scope.managers) {
+            if (!$scope.managers[i].address || !$scope.managers[i].port) {
+              $scope.loading = false;
+              toastService.error('Cannot save. Please resolve errors on page.');
+              return;
             }
           }
-        }
+          // Iterate in reverse so can splice
+          // https://stackoverflow.com/questions/9882284/looping-through-array-and-removing-items-without-breaking-for-loop
+          let i = $scope.managers.length;
+          while (i--) {
+            // If the manager does not have a 'path', it is a new manager
+            // and needs to be created
+            if (!$scope.managers[i].path) {
+              promises.push(
+                addManager($scope.managers[i].address, $scope.managers[i].port)
+              );
+            } else {
+              if ($scope.managers[i].updateAddress) {
+                promises.push(
+                  setManagerAddress(
+                    $scope.managers[i].path,
+                    $scope.managers[i].address
+                  )
+                );
+              }
+              if ($scope.managers[i].updatePort) {
+                promises.push(
+                  setManagerPort(
+                    $scope.managers[i].path,
+                    $scope.managers[i].port
+                  )
+                );
+              }
+            }
+          }
 
-        // Add delete promises last since we might be adding to
-        // managersToDelete above
-        for (var i in $scope.managersToDelete) {
-          promises.push(deleteManager($scope.managersToDelete[i]));
-        }
+          // Add delete promises last since we might be adding to
+          // managersToDelete above
+          for (let i in $scope.managersToDelete) {
+            promises.push(deleteManager($scope.managersToDelete[i]));
+          }
 
-        $q.all(promises)
+          $q.all(promises)
             .then(
-                function() {
-                  toastService.success('SNMP settings have been saved.');
-                },
-                function(errors) {
-                  toastService.error('Unable to set SNMP Managers.');
-                  console.log(JSON.stringify(errors));
-                })
+              function() {
+                toastService.success('SNMP settings have been saved.');
+              },
+              function(errors) {
+                toastService.error('Unable to set SNMP Managers.');
+                console.log(JSON.stringify(errors));
+              }
+            )
             .finally(function() {
               $scope.loading = false;
             });
-      };
+        };
 
-      function addManager(address, port) {
-        return APIUtils.addSNMPManager(address, port);
-      }
+        function addManager(address, port) {
+          return APIUtils.addSNMPManager(address, port);
+        }
 
-      function deleteManager(path) {
-        return APIUtils.deleteObject(path);
-      }
+        function deleteManager(path) {
+          return APIUtils.deleteObject(path);
+        }
 
-      function setManagerAddress(path, address) {
-        return APIUtils.setSNMPManagerAddress(path, address);
-      }
+        function setManagerAddress(path, address) {
+          return APIUtils.setSNMPManagerAddress(path, address);
+        }
 
-      function setManagerPort(path, port) {
-        return APIUtils.setSNMPManagerPort(path, port);
+        function setManagerPort(path, port) {
+          return APIUtils.setSNMPManagerPort(path, port);
+        }
       }
-    }
-  ]);
-})(angular);
+    ]);
+  })(window.angular);
--- app/login/controllers/login-controller.js
+++ app/login/controllers/login-controller.js
@@ -6,55 +6,62 @@
  * @name LoginController
  */
 
-window.angular && (function(angular) {
-  'use strict';
+window.angular &&
+  (function(angular) {
+    'use strict';
 
-  angular.module('app.login').controller('LoginController', [
-    '$scope',
-    '$window',
-    'dataService',
-    'userModel',
-    '$location',
-    function($scope, $window, dataService, userModel, $location) {
-      $scope.dataService = dataService;
-      $scope.serverUnreachable = false;
-      $scope.invalidCredentials = false;
-      $scope.host = $scope.dataService.host.replace(/^https?\:\/\//ig, '');
-
-      $scope.tryLogin = function(host, username, password, event) {
-        // keyCode 13 is the 'Enter' button. If the user hits 'Enter' while in
-        // one of the 3 fields, attempt to log in.
-        if (event.keyCode === 13) {
-          $scope.login(host, username, password);
-        }
-      };
-      $scope.login = function(host, username, password) {
+    angular.module('app.login').controller('LoginController', [
+      '$scope',
+      '$window',
+      'dataService',
+      'userModel',
+      '$location',
+      function($scope, $window, dataService, userModel, $location) {
+        $scope.dataService = dataService;
         $scope.serverUnreachable = false;
         $scope.invalidCredentials = false;
-        if (!username || username == '' || !password || password == '' ||
-            !host || host == '') {
-          return false;
-        } else {
-          $scope.dataService.setHost(host);
-          userModel.login(username, password, function(status, description) {
-            if (status) {
-              $scope.$emit('user-logged-in', {});
-              var next = $location.search().next;
-              if (next === undefined || next == null) {
-                $window.location.hash = '#/overview/server';
+        $scope.host = $scope.dataService.host.replace(/^https?\:\/\//gi, '');
+
+        $scope.tryLogin = function(host, username, password, event) {
+          // keyCode 13 is the 'Enter' button. If the user hits 'Enter' while in
+          // one of the 3 fields, attempt to log in.
+          if (event.keyCode === 13) {
+            $scope.login(host, username, password);
+          }
+        };
+        $scope.login = function(host, username, password) {
+          $scope.serverUnreachable = false;
+          $scope.invalidCredentials = false;
+          if (
+            !username ||
+            username == '' ||
+            !password ||
+            password == '' ||
+            !host ||
+            host == ''
+          ) {
+            return false;
+          } else {
+            $scope.dataService.setHost(host);
+            userModel.login(username, password, function(status, description) {
+              if (status) {
+                $scope.$emit('user-logged-in', {});
+                var next = $location.search().next;
+                if (next === undefined || next == null) {
+                  $window.location.hash = '#/overview/server';
+                } else {
+                  $window.location.href = next;
+                }
               } else {
-                $window.location.href = next;
+                if (description === 'Unauthorized') {
+                  $scope.invalidCredentials = true;
+                } else {
+                  $scope.serverUnreachable = true;
+                }
               }
-            } else {
-              if (description === 'Unauthorized') {
-                $scope.invalidCredentials = true;
-              } else {
-                $scope.serverUnreachable = true;
-              }
-            }
-          });
-        }
-      };
-    },
-  ]);
-})(angular);
+            });
+          }
+        };
+      }
+    ]);
+  })(angular);
