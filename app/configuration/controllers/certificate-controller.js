92edc6f422fa8da4cb8b78afc502a61f4530fe1e
miramurali23 <miramurali23@gmail.com>
2019-06-10 15:09:27 -0500

    Create csr modal
    The CSR Modal allows users to generate a CSR code. Once the user types in the
    necessary information to generate the CSR code, that modal will then render the
    code and the user will be able to either copy the code or download the code
    in a txt file.
    
    Tested: loaded onto Witherspoon and able to view on certificate management page.
     Click on generate csr and type in necessary (or any additional info), then click on generate CSR.
     The CSR code is then visible and is able to be copied or downloaded. In error state in
    which CSR code is unable to generate, the modal closes and an error toast message appears.
    
    Signed-off-by: miramurali23 <miramurali23@gmail.com>
    Change-Id: I3cca09c494357496166164b5ee8ff99250ef981d

--- app/configuration/controllers/certificate-controller.js
+++ app/configuration/controllers/certificate-controller.js
@@ -6,77 +6,90 @@
  * @name certificateController
  */
 
-window.angular && (function(angular) {
+window.angular && (function (angular) {
   'use strict';
 
   angular.module('app.configuration').controller('certificateController', [
-    '$scope', 'APIUtils', '$q', 'Constants', 'toastService',
-    function($scope, APIUtils, $q, Constants, toastService) {
+    '$scope', 'APIUtils', '$q', 'Constants', 'toastService', '$timeout',
+    function ($scope, APIUtils, $q, Constants, toastService, $timeout) {
       $scope.loading = false;
       $scope.certificates = [];
       $scope.availableCertificateTypes = [];
+      $scope.allCertificateTypes = Constants.CERTIFICATE_TYPES;
       $scope.addCertificateModal = false;
+      $scope.addCSRModal = false;
       $scope.newCertificate = {};
+      $scope.newCSR = {};
       $scope.submitted = false;
+      $scope.csrSubmitted = false;
+      $scope.csrCode = "";
+      $scope.displayCSRCode = false;
+      $scope.keyBitLength = Constants.CERTIFICATE.KEY_BIT_LENGTH;
+      $scope.keyUsage = Constants.CERTIFICATE.KEY_USAGE;
+      $scope.keyPairAlgorithm = Constants.CERTIFICATE.KEY_PAIR_ALGORITHM;
+      $scope.keyCurveId = Constants.CERTIFICATE.KEY_CURVE_ID;
+      $scope.countryList = Constants.COUNTRIES;
+      $scope.countryCodeMaxLength = 2;
 
-      $scope.loadCertificates = function() {
+
+
+      $scope.loadCertificates = function () {
         $scope.certificates = [];
         $scope.availableCertificateTypes = Constants.CERTIFICATE_TYPES;
         $scope.loading = true;
         // Use Certificate Service to get the locations of all the certificates,
         // then add a promise for fetching each certificate
         APIUtils.getCertificateLocations().then(
-            function(data) {
-              var promises = [];
-              var locations = data.Links.Certificates;
-              for (var i in locations) {
-                var location = locations[i];
-                promises.push(getCertificatePromise(location['@odata.id']));
-              }
-              $q.all(promises)
-                  .catch(function(error) {
-                    toastService.error('Failed to load certificates.');
-                    console.log(JSON.stringify(error));
-                  })
-                  .finally(function() {
-                    $scope.loading = false;
-                  });
-            },
-            function(error) {
-              $scope.loading = false;
-              $scope.availableCertificateTypes = [];
-              toastService.error('Failed to load certificates.');
-              console.log(JSON.stringify(error));
-            });
+          function (data) {
+            var promises = [];
+            var locations = data.Links.Certificates;
+            for (var i in locations) {
+              var location = locations[i];
+              promises.push(getCertificatePromise(location['@odata.id']));
+            }
+            $q.all(promises)
+              .catch(function (error) {
+                toastService.error('Failed to load certificates.');
+                console.log(JSON.stringify(error));
+              })
+              .finally(function () {
+                $scope.loading = false;
+              });
+          },
+          function (error) {
+            $scope.loading = false;
+            $scope.availableCertificateTypes = [];
+            toastService.error('Failed to load certificates.');
+            console.log(JSON.stringify(error));
+          });
       };
 
-      $scope.uploadCertificate = function() {
+      $scope.uploadCertificate = function () {
         if ($scope.newCertificate.file.name.split('.').pop() !== 'pem') {
           toastService.error('Certificate must be a .pem file.');
           return;
         }
         $scope.addCertificateModal = false;
-        APIUtils
-            .addNewCertificate(
-                $scope.newCertificate.file, $scope.newCertificate.selectedType)
-            .then(
-                function(data) {
-                  toastService.success(
-                      $scope.newCertificate.selectedType.Description +
-                      ' was uploaded.');
-                  $scope.newCertificate = {};
-                  $scope.loadCertificates();
-                },
-                function(error) {
-                  toastService.error(
-                      $scope.newCertificate.selectedType.Description +
-                      ' failed upload.');
-                  console.log(JSON.stringify(error));
-                });
+        APIUtils.addNewCertificate(
+          $scope.newCertificate.file, $scope.newCertificate.selectedType)
+          .then(
+            function (data) {
+              toastService.success(
+                $scope.newCertificate.selectedType.Description +
+                ' was uploaded.');
+              $scope.newCertificate = {};
+              $scope.loadCertificates();
+            },
+            function (error) {
+              toastService.error(
+                $scope.newCertificate.selectedType.Description +
+                ' failed upload.');
+              console.log(JSON.stringify(error));
+            });
       };
 
-      var getCertificatePromise = function(url) {
-        var promise = APIUtils.getCertificate(url).then(function(data) {
+      var getCertificatePromise = function (url) {
+        var promise = APIUtils.getCertificate(url).then(function (data) {
           var certificate = data;
           isExpiring(certificate);
           updateAvailableTypes(certificate);
@@ -85,9 +98,7 @@ window.angular && (function(angular) {
         return promise;
       };
 
-      var isExpiring = function(certificate) {
-        // if ValidNotAfter is less than or equal to 30 days from today
-        // (2592000000), isExpiring. If less than or equal to 0, is expired.
+      var isExpiring = function (certificate) {
         var difference = certificate.ValidNotAfter - new Date();
         if (difference <= 0) {
           certificate.isExpired = true;
@@ -99,17 +110,107 @@ window.angular && (function(angular) {
         }
       };
 
-      var updateAvailableTypes = function(certificate) {
+      //add optional name
+      $scope.names = [];
+      $scope.addRow = function () {
+        $scope.names.push({ Value: '' })
+      }
+
+      //remove optional name row
+      $scope.deleteRow = function (index) {
+        $scope.names.splice(index, 1);
+        if ($scope.names.length == 0) {
+          $scope.names = [];
+        }
+      }
+
+
+      //create a CSR object to send to the backend
+      $scope.getCSRCode = function () {
+        var addCSR = {};
+        let alternativeNames = $scope.names.map(name => name.Value);
+
+        //if user provided a first alternative name then push to alternative names array
+        $scope.newCSR.firstAlternativeName ? alternativeNames.push($scope.newCSR.firstAlternativeName) : $scope.newCSR.firstAlternativeName = "";
+
+
+        addCSR.CertificateCollection = { "@odata.id": $scope.newCSR.certificateCollection.location || "" };
+        addCSR.CommonName = $scope.newCSR.commonName;
+        addCSR.ContactPerson = $scope.newCSR.contactPerson || "";
+        addCSR.City = $scope.newCSR.city || "";
+        addCSR.AlternativeNames = alternativeNames || [];
+        addCSR.ChallengePassword = $scope.newCSR.challengePassword || "";
+        addCSR.Email = $scope.newCSR.emailAddress || "";
+        addCSR.Country = $scope.newCSR.countryCode.code || "";
+        addCSR.KeyCurveId = $scope.newCSR.keyCurveId || "";
+        addCSR.KeyBitLength = $scope.newCSR.keyBitLength
+        addCSR.KeyPairAlgorithm = $scope.newCSR.keyPairAlgorithm || "";
+        addCSR.KeyUsage = $scope.newCSR.keyUsage || [];
+        addCSR.Organization = $scope.newCSR.organization || "";
+        addCSR.OrganizationalUnit = $scope.newCSR.companyUnit || "";
+        addCSR.State = $scope.newCSR.state || "";
+
+        console.log('addCSR.Country: ', addCSR.Country);
+
+
+        APIUtils.createCSRCertificate(addCSR).then(
+          function (data) {
+            $scope.displayCSRCode = true;
+            $scope.csrCode = data;
+          },
+          function (error) {
+            $scope.addCSRModal = false;
+            toastService.error('Unable to generate CSR. Log out and try again.');
+            console.log(JSON.stringify(error));
+          }
+        )
+      }
+
+      //resetting the modal when user clicks cancel/closes the modal
+      $scope.resetModal = function () {
+        $scope.addCSRModal = false;
+        $scope.displayCSRCode = false;
+        $scope.newCSR.certificateCollection = $scope.selectOption;
+        $scope.newCSR.commonName = "";
+        $scope.newCSR.contactPerson = "";
+        $scope.newCSR.city = "";
+        $scope.names = [];
+        $scope.newCSR.challengePassword = "";
+        $scope.newCSR.emailAddress = "";
+        $scope.newCSR.countryCode = "";
+        $scope.newCSR.keyCurveId = "";
+        $scope.newCSR.firstAlternativeName = "";
+        $scope.newCSR.keyBitLength = $scope.selectOption;
+        $scope.newCSR.keyPairAlgorithm = $scope.selectOption;
+        $scope.newCSR.keyUsage = [];
+        $scope.newCSR.organization = "";
+        $scope.newCSR.companyUnit = "";
+        $scope.newCSR.state = "";
+      }
+
+      //copies the CSR code
+      $scope.copySuccess = function (event) {
+        $scope.copied = true;
+        $timeout(function () {
+          $scope.copied = false;
+        }, 5000);
+      };
+      $scope.copyFailed = function (err) {
+        console.error('Error!', err);
+      };
+
+
+      var updateAvailableTypes = function (certificate) {
         // TODO: at this time only one of each type of certificate is allowed.
         // When this changes, this will need to be updated.
         // Removes certificate type from available types to be added.
         $scope.availableCertificateTypes =
-            $scope.availableCertificateTypes.filter(function(type) {
-              return type.Description !== certificate.Description;
-            });
+          $scope.availableCertificateTypes.filter(function (type) {
+            return type.Description !== certificate.Description;
+          });
       };
 
-      $scope.getDays = function(endDate) {
+      $scope.getDays = function (endDate) {
         // finds number of days until certificate expiration
         var ms = endDate - new Date();
         return Math.floor(ms / (24 * 60 * 60 * 1000));
