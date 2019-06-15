a7ea7bf71d9def1594ba044bee60f659598296fc
Derick Montague <derick.montague@ibm.com>
2019-06-14 23:37:38 -0500

    Add ESLint and Prettier
    Using Google JavaScript style guide through the
    Google eslint shared config along with Prettier
    eslint plugin and Prettier recommeneded settings
    to format and lint our JS and SCSS files. Below is
    a list of the errors/warnings and what was needed
    to manually resolve a small set of them. In testing
    we found the user of a for...in loop to loop through
    an array when the best practice is to use a forEach
    loop.
    
    There is a small set of rules that the Google JS
    Style Guide sets as errors that we are setting to
    warnings until we have more time to address them.
    We are also allowing Prettier recommended to
    override the Google style guides "indent
    continuation lines" rule.
    https://google.github.io/styleguide/jsguide.html#formatting-indent
    
    To address CI, we have added NPM to the Docker
    container and are using the format-code.sh script
    to install only the modules necessary to test the
    code.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: If7a7a63754a35a35e85daa748a499627cec59d7e

--- app/server-control/controllers/power-operations-controller.js
+++ app/server-control/controllers/power-operations-controller.js
@@ -6,46 +6,64 @@
  * @name powerOperationsController
  */
 
-window.angular && (function(angular) {
-  'use strict';
+window.angular &&
+  (function(angular) {
+    'use strict';
 
-  angular.module('app.serverControl').controller('powerOperationsController', [
-    '$scope', 'APIUtils', 'dataService', 'Constants', '$interval', '$q',
-    'toastService',
-    function(
-        $scope, APIUtils, dataService, Constants, $interval, $q, toastService) {
-      $scope.dataService = dataService;
-      // Is a || of the other 4 "confirm" variables to ensure only
-      // one confirm is shown at a time.
-      $scope.confirm = false;
-      $scope.confirmWarmReboot = false;
-      $scope.confirmColdReboot = false;
-      $scope.confirmOrderlyShutdown = false;
-      $scope.confirmImmediateShutdown = false;
-      $scope.loading = true;
+    angular
+      .module('app.serverControl')
+      .controller('powerOperationsController', [
+        '$scope',
+        'APIUtils',
+        'dataService',
+        'Constants',
+        '$interval',
+        '$q',
+        'toastService',
+        function(
+          $scope,
+          APIUtils,
+          dataService,
+          Constants,
+          $interval,
+          $q,
+          toastService
+        ) {
+          $scope.dataService = dataService;
+          // Is a || of the other 4 "confirm" variables to ensure only
+          // one confirm is shown at a time.
+          $scope.confirm = false;
+          $scope.confirmWarmReboot = false;
+          $scope.confirmColdReboot = false;
+          $scope.confirmOrderlyShutdown = false;
+          $scope.confirmImmediateShutdown = false;
+          $scope.loading = true;
 
-      // When a power operation is in progress, set to true,
-      // when a power operation completes (success/fail) set to false.
-      // This property is used to show/hide the 'in progress' message
-      // in markup.
-      $scope.operationPending = false;
+          // When a power operation is in progress, set to true,
+          // when a power operation completes (success/fail) set to false.
+          // This property is used to show/hide the 'in progress' message
+          // in markup.
+          $scope.operationPending = false;
 
-      /**
-       * Checks the host status provided by the dataService using an
-       * interval timer
-       * @param {string} statusType : host status type to check for
-       * @param {number} timeout : timeout limit, defaults to 5 minutes
-       * @param {string} error : error message, defaults to 'Time out'
-       * @returns {Promise} : returns a deferred promise that will be fulfilled
-       * if the status is met or be rejected if the timeout is reached
-       */
-      var checkHostStatus =
-          (statusType, timeout = 300000, error = 'Time out.') => {
+          /**
+           * Checks the host status provided by the dataService using an
+           * interval timer
+           * @param {string} statusType : host status type to check for
+           * @param {number} timeout : timeout limit, defaults to 5 minutes
+           * @param {string} error : error message, defaults to 'Time out'
+           * @return {Promise} : returns a deferred promise that will be fulfilled
+           * if the status is met or be rejected if the timeout is reached
+           */
+          var checkHostStatus = (
+            statusType,
+            timeout = 300000,
+            error = 'Time out.'
+          ) => {
             const deferred = $q.defer();
             const start = new Date();
             const checkHostStatusInverval = $interval(() => {
-              let now = new Date();
-              let timePassed = now.getTime() - start.getTime();
+              const now = new Date();
+              const timePassed = now.getTime() - start.getTime();
               if (timePassed > timeout) {
                 deferred.reject(error);
                 $interval.cancel(checkHostStatusInverval);
@@ -58,8 +76,8 @@ window.angular && (function(angular) {
             return deferred.promise;
           };
 
-      APIUtils.getLastPowerTime()
-          .then(
+          APIUtils.getLastPowerTime()
+            .then(
               function(data) {
                 if (data.data == 0) {
                   $scope.powerTime = 'not available';
@@ -69,186 +87,203 @@ window.angular && (function(angular) {
               },
               function(error) {
                 console.log(JSON.stringify(error));
+              }
+            )
+            .finally(function() {
+              $scope.loading = false;
+            });
+
+          $scope.toggleState = function() {
+            dataService.server_state =
+              dataService.server_state == 'Running' ? 'Off' : 'Running';
+          };
+
+          /**
+           * Initiate Power on
+           */
+          $scope.powerOn = () => {
+            $scope.operationPending = true;
+            dataService.setUnreachableState();
+            APIUtils.hostPowerOn()
+              .then(() => {
+                // Check for on state
+                return checkHostStatus(
+                  Constants.HOST_STATE_TEXT.on,
+                  Constants.TIMEOUT.HOST_ON,
+                  Constants.MESSAGES.POLL.HOST_ON_TIMEOUT
+                );
               })
-          .finally(function() {
-            $scope.loading = false;
-          });
+              .catch(error => {
+                console.log(error);
+                toastService.error(Constants.MESSAGES.POWER_OP.POWER_ON_FAILED);
+              })
+              .finally(() => {
+                $scope.operationPending = false;
+              });
+          };
 
-      $scope.toggleState = function() {
-        dataService.server_state =
-            (dataService.server_state == 'Running') ? 'Off' : 'Running';
-      };
+          /**
+           * Initiate Orderly reboot
+           * Attempts to stop all software
+           */
+          $scope.warmReboot = () => {
+            $scope.operationPending = true;
+            dataService.setUnreachableState();
+            APIUtils.hostReboot()
+              .then(() => {
+                // Check for off state
+                return checkHostStatus(
+                  Constants.HOST_STATE_TEXT.off,
+                  Constants.TIMEOUT.HOST_OFF,
+                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT
+                );
+              })
+              .then(() => {
+                // Check for on state
+                return checkHostStatus(
+                  Constants.HOST_STATE_TEXT.on,
+                  Constants.TIMEOUT.HOST_ON,
+                  Constants.MESSAGES.POLL.HOST_ON_TIMEOUT
+                );
+              })
+              .catch(error => {
+                console.log(error);
+                toastService.error(
+                  Constants.MESSAGES.POWER_OP.WARM_REBOOT_FAILED
+                );
+              })
+              .finally(() => {
+                $scope.operationPending = false;
+              });
+          };
 
-      /**
-       * Initiate Power on
-       */
-      $scope.powerOn = () => {
-        $scope.operationPending = true;
-        dataService.setUnreachableState();
-        APIUtils.hostPowerOn()
-            .then(() => {
-              // Check for on state
-              return checkHostStatus(
-                  Constants.HOST_STATE_TEXT.on, Constants.TIMEOUT.HOST_ON,
-                  Constants.MESSAGES.POLL.HOST_ON_TIMEOUT);
-            })
-            .catch((error) => {
-              console.log(error);
-              toastService.error(Constants.MESSAGES.POWER_OP.POWER_ON_FAILED);
-            })
-            .finally(() => {
-              $scope.operationPending = false;
-            })
-      };
+          $scope.warmRebootConfirm = function() {
+            if ($scope.confirm) {
+              // If another "confirm" is already shown return
+              return;
+            }
+            $scope.confirm = true;
+            $scope.confirmWarmReboot = true;
+          };
 
-      /**
-       * Initiate Orderly reboot
-       * Attempts to stop all software
-       */
-      $scope.warmReboot = () => {
-        $scope.operationPending = true;
-        dataService.setUnreachableState();
-        APIUtils.hostReboot()
-            .then(() => {
-              // Check for off state
-              return checkHostStatus(
-                  Constants.HOST_STATE_TEXT.off, Constants.TIMEOUT.HOST_OFF,
-                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT);
-            })
-            .then(() => {
-              // Check for on state
-              return checkHostStatus(
-                  Constants.HOST_STATE_TEXT.on, Constants.TIMEOUT.HOST_ON,
-                  Constants.MESSAGES.POLL.HOST_ON_TIMEOUT);
-            })
-            .catch((error) => {
-              console.log(error);
-              toastService.error(
-                  Constants.MESSAGES.POWER_OP.WARM_REBOOT_FAILED);
-            })
-            .finally(() => {
-              $scope.operationPending = false;
-            })
-      };
-
-      $scope.warmRebootConfirm = function() {
-        if ($scope.confirm) {
-          // If another "confirm" is already shown return
-          return;
-        }
-        $scope.confirm = true;
-        $scope.confirmWarmReboot = true;
-      };
-
-      /**
-       * Initiate Immediate reboot
-       * Does not attempt to stop all software
-       */
-      $scope.coldReboot = () => {
-        $scope.operationPending = true;
-        dataService.setUnreachableState();
-        APIUtils.chassisPowerOff()
-            .then(() => {
-              // Check for off state
-              return checkHostStatus(
+          /**
+           * Initiate Immediate reboot
+           * Does not attempt to stop all software
+           */
+          $scope.coldReboot = () => {
+            $scope.operationPending = true;
+            dataService.setUnreachableState();
+            APIUtils.chassisPowerOff()
+              .then(() => {
+                // Check for off state
+                return checkHostStatus(
                   Constants.HOST_STATE_TEXT.off,
                   Constants.TIMEOUT.HOST_OFF_IMMEDIATE,
-                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT);
-            })
-            .then(() => {
-              return APIUtils.hostPowerOn();
-            })
-            .then(() => {
-              // Check for on state
-              return checkHostStatus(
-                  Constants.HOST_STATE_TEXT.on, Constants.TIMEOUT.HOST_ON,
-                  Constants.MESSAGES.POLL.HOST_ON_TIMEOUT);
-            })
-            .catch((error) => {
-              console.log(error);
-              toastService.error(
-                  Constants.MESSAGES.POWER_OP.COLD_REBOOT_FAILED);
-            })
-            .finally(() => {
-              $scope.operationPending = false;
-            })
-      };
+                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT
+                );
+              })
+              .then(() => {
+                return APIUtils.hostPowerOn();
+              })
+              .then(() => {
+                // Check for on state
+                return checkHostStatus(
+                  Constants.HOST_STATE_TEXT.on,
+                  Constants.TIMEOUT.HOST_ON,
+                  Constants.MESSAGES.POLL.HOST_ON_TIMEOUT
+                );
+              })
+              .catch(error => {
+                console.log(error);
+                toastService.error(
+                  Constants.MESSAGES.POWER_OP.COLD_REBOOT_FAILED
+                );
+              })
+              .finally(() => {
+                $scope.operationPending = false;
+              });
+          };
 
-      $scope.coldRebootConfirm = function() {
-        if ($scope.confirm) {
-          return;
-        }
-        $scope.confirm = true;
-        $scope.confirmColdReboot = true;
-      };
+          $scope.coldRebootConfirm = function() {
+            if ($scope.confirm) {
+              return;
+            }
+            $scope.confirm = true;
+            $scope.confirmColdReboot = true;
+          };
 
-      /**
-       * Initiate Orderly shutdown
-       * Attempts to stop all software
-       */
-      $scope.orderlyShutdown = () => {
-        $scope.operationPending = true;
-        dataService.setUnreachableState();
-        APIUtils.hostPowerOff()
-            .then(() => {
-              // Check for off state
-              return checkHostStatus(
-                  Constants.HOST_STATE_TEXT.off, Constants.TIMEOUT.HOST_OFF,
-                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT);
-            })
-            .catch((error) => {
-              console.log(error);
-              toastService.error(
-                  Constants.MESSAGES.POWER_OP.ORDERLY_SHUTDOWN_FAILED);
-            })
-            .finally(() => {
-              $scope.operationPending = false;
-            })
-      };
+          /**
+           * Initiate Orderly shutdown
+           * Attempts to stop all software
+           */
+          $scope.orderlyShutdown = () => {
+            $scope.operationPending = true;
+            dataService.setUnreachableState();
+            APIUtils.hostPowerOff()
+              .then(() => {
+                // Check for off state
+                return checkHostStatus(
+                  Constants.HOST_STATE_TEXT.off,
+                  Constants.TIMEOUT.HOST_OFF,
+                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT
+                );
+              })
+              .catch(error => {
+                console.log(error);
+                toastService.error(
+                  Constants.MESSAGES.POWER_OP.ORDERLY_SHUTDOWN_FAILED
+                );
+              })
+              .finally(() => {
+                $scope.operationPending = false;
+              });
+          };
 
-      $scope.orderlyShutdownConfirm = function() {
-        if ($scope.confirm) {
-          return;
-        }
-        $scope.confirm = true;
-        $scope.confirmOrderlyShutdown = true;
-      };
+          $scope.orderlyShutdownConfirm = function() {
+            if ($scope.confirm) {
+              return;
+            }
+            $scope.confirm = true;
+            $scope.confirmOrderlyShutdown = true;
+          };
 
-      /**
-       * Initiate Immediate shutdown
-       * Does not attempt to stop all software
-       */
-      $scope.immediateShutdown = () => {
-        $scope.operationPending = true;
-        dataService.setUnreachableState();
-        APIUtils.chassisPowerOff()
-            .then(() => {
-              // Check for off state
-              return checkHostStatus(
+          /**
+           * Initiate Immediate shutdown
+           * Does not attempt to stop all software
+           */
+          $scope.immediateShutdown = () => {
+            $scope.operationPending = true;
+            dataService.setUnreachableState();
+            APIUtils.chassisPowerOff()
+              .then(() => {
+                // Check for off state
+                return checkHostStatus(
                   Constants.HOST_STATE_TEXT.off,
                   Constants.TIMEOUT.HOST_OFF_IMMEDIATE,
-                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT);
-            })
-            .then(() => {
-              dataService.setPowerOffState();
-            })
-            .catch((error) => {
-              console.log(error);
-              toastService.error(
-                  Constants.MESSAGES.POWER_OP.IMMEDIATE_SHUTDOWN_FAILED);
-            })
-            .finally(() => {
-              $scope.operationPending = false;
-            })
-      };
+                  Constants.MESSAGES.POLL.HOST_OFF_TIMEOUT
+                );
+              })
+              .then(() => {
+                dataService.setPowerOffState();
+              })
+              .catch(error => {
+                console.log(error);
+                toastService.error(
+                  Constants.MESSAGES.POWER_OP.IMMEDIATE_SHUTDOWN_FAILED
+                );
+              })
+              .finally(() => {
+                $scope.operationPending = false;
+              });
+          };
 
-      $scope.immediateShutdownConfirm = function() {
-        if ($scope.confirm) {
-          return;
-        }
-        $scope.confirm = true;
-        $scope.confirmImmediateShutdown = true;
-      };
-    }
-  ]);
-})(angular);
+          $scope.immediateShutdownConfirm = function() {
+            if ($scope.confirm) {
+              return;
+            }
+            $scope.confirm = true;
+            $scope.confirmImmediateShutdown = true;
+          };
+        },
+      ]);
+  })(angular);
