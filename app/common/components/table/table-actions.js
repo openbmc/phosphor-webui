1f40d7a0f11500111a253f9ac34c1a0d6676bd31
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-08-01 11:28:15 -0500

    Add bcmTableActions component
    Creating a separate table actions component to allow
    row action customizations like disabling certain
    actions and rendering different icons.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I85e96045af27701f5ecc4af9bf824e248abccbf5

---
+++ app/common/components/table/table-actions.js
@@ -0,0 +1,91 @@
+window.angular && (function(angular) {
+  'use strict';
+
+  /**
+   *
+   * bmcTableActions component
+   *
+   * To use:
+   * The <bmc-table-actions> component expects an 'actions' attribute
+   * that should be an array of action objects.
+   * Each action object should have 'type', 'enabled', and 'file'
+   * properties.
+   *
+   * actions: [
+   *  {type: 'Edit', enabled: true, file: 'icon-edit.svg'},
+   *  {type: 'Delete', enabled: false, file: 'icon-trashcan.svg'}
+   * ]
+   *
+   * The 'type' property is a string value that will be emitted to the
+   * parent component when clicked.
+   *
+   * The 'enabled' property is a boolean that will enable/disable
+   * the button.
+   *
+   * The 'file' property is a string value of the filename of the svg icon
+   * to provide <icon> directive.
+   *
+   */
+
+  const controller = function() {
+    /**
+     * Set defaults if properties undefined
+     * @param {[]} actions
+     */
+    const setActions = (actions = []) => {
+      return actions
+          .map((action) => {
+            if (action.type === undefined) {
+              return;
+            }
+            if (action.file === undefined) {
+              action.file = null;
+            }
+            if (action.enabled === undefined) {
+              action.enabled = true;
+            }
+            return action;
+          })
+          .filter((action) => action);
+    };
+
+    /**
+     * Callback when button action clicked
+     * Emits the action type to the parent component
+     */
+    this.onClick = (action) => {
+      this.emitAction({action});
+    };
+
+    /**
+     * onInit Component lifecycle hook
+     */
+    this.$onInit = () => {
+      this.actions = setActions(this.actions);
+    };
+  };
+
+  /**
+   * Component template
+   */
+  const template = `
+    <button
+      class="btn  btn-tertiary"
+      type="button"
+      aria-label="action.type"
+      ng-repeat="action in $ctrl.actions"
+      ng-disabled="!action.enabled"
+      ng-click="$ctrl.onClick(action.type)">
+      <icon ng-if="action.file !== null" ng-file="{{action.file}}"></icon>
+      <span ng-if="action.file === null">{{action.type}}</span>
+    </button>`
+
+  /**
+   * Register bmcTableActions component
+   */
+  angular.module('app.common.components').component('bmcTableActions', {
+    controller,
+    template,
+    bindings: {actions: '<', emitAction: '&'}
+  })
+})(window.angular);
