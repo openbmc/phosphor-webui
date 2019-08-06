74e561e134d0c74aacd5d08492c2f81a8c798439
Derick Montague <derick.montague@ibm.com>
2019-08-06 14:47:47 -0500

    Add ESLint and Prettier
    Using Google JavaScript style guide through the
    Google eslint shared config along with Prettier
    eslint plugin and Prettier recommeneded settings
    to format and lint our JS and SCSS files. Below is
    a list of the errors/warnings and what was needed
    to manually resolve a small set of them. In testing
    we found the user of a for...in loop to loop through
    an array when the best practice is to use a forEach
    loop.
    
    There is a small set of rules that the Google JS
    Style Guide sets as errors that we are setting to
    warnings until we have more time to address them.
    We are also allowing Prettier recommended to
    override the Google style guides "indent
    continuation lines" rule.
    https://google.github.io/styleguide/jsguide.html#formatting-indent
    
    To address CI, we have added NPM to the Docker
    container and are using the format-code.sh script
    to install only the modules necessary to test the
    code.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: If7a7a63754a35a35e85daa748a499627cec59d7e

--- app/server-control/controllers/kvm-controller.js
+++ app/server-control/controllers/kvm-controller.js
@@ -8,48 +8,56 @@
 
 import RFB from '@novnc/novnc/core/rfb.js';
 
-window.angular && (function(angular) {
-  'use strict';
+window.angular &&
+  (function(angular) {
+    'use strict';
 
-  angular.module('app.serverControl').controller('kvmController', [
-    '$scope', '$location', '$log',
-    function($scope, $location, $log) {
-      var rfb;
+    angular.module('app.serverControl').controller('kvmController', [
+      '$scope',
+      '$location',
+      '$log',
+      function($scope, $location, $log) {
+        let rfb;
 
-      $scope.$on('$destroy', function() {
-        if (rfb) {
-          rfb.disconnect();
+        $scope.$on('$destroy', function() {
+          if (rfb) {
+            rfb.disconnect();
+          }
+        });
+
+        function sendCtrlAltDel() {
+          rfb.sendCtrlAltDel();
+          return false;
         }
-      });
 
-      function sendCtrlAltDel() {
-        rfb.sendCtrlAltDel();
-        return false;
-      };
+        function connected(e) {
+          $log.debug('RFB Connected');
+        }
+        function disconnected(e) {
+          $log.debug('RFB disconnected');
+        }
 
-      function connected(e) {
-        $log.debug('RFB Connected');
-      }
-      function disconnected(e) {
-        $log.debug('RFB disconnected');
-      }
+        const host = $location.host();
+        const port = $location.port();
+        const target = angular.element(
+          document.querySelector('#noVNC_container')
+        )[0];
 
-      var host = $location.host();
-      var port = $location.port();
-      var target =
-          angular.element(document.querySelector('#noVNC_container'))[0];
+        try {
+          rfb = new RFB(target, 'wss://' + host + ':' + port + '/kvm/0', {});
 
-      try {
-        rfb = new RFB(target, 'wss://' + host + ':' + port + '/kvm/0', {});
-
-        rfb.addEventListener('connect', connected);
-        rfb.addEventListener('disconnect', disconnected);
-      } catch (exc) {
-        $log.error(exc);
-        updateState(
-            null, 'fatal', null, 'Unable to create RFB client -- ' + exc);
-        return;  // don't continue trying to connect
-      };
-    }
-  ]);
-})(angular);
+          rfb.addEventListener('connect', connected);
+          rfb.addEventListener('disconnect', disconnected);
+        } catch (exc) {
+          $log.error(exc);
+          updateState(
+            null,
+            'fatal',
+            null,
+            'Unable to create RFB client -- ' + exc
+          );
+          return; // don't continue trying to connect
+        }
+      },
+    ]);
+  })(angular);
