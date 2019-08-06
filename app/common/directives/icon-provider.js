f5e6410b9ba4a36c7b5245e24a76f2e9821e5233
Derick Montague <derick.montague@ibm.com>
2019-08-06 14:47:47 -0500

    Add ESLint and Prettier
    Using Google JavaScript style guide through the
    Google eslint shared config along with Prettier
    eslint plugin and Prettier recommeneded settings
    to format and lint our JS and SCSS files. In testing
    we found a few instances of a for...in loop used to
    iterate over an array. It is a best practice is to
    use a forEach loop for arrays and a for-in for objects.
    
    There is a small set of rules that the Google JS
    Style Guide sets as errors that we are setting to
    warnings until we have more time to address them.
    We are also allowing Prettier recommended to
    override the Google style guides "indent
    continuation lines" rule.
    https://google.github.io/styleguide/jsguide.html#formatting-indent
    
    To address CI, we have added NPM to the Docker
    container and are using the format-code.sh script
    to install only the modules necessary to test the
    code.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: If7a7a63754a35a35e85daa748a499627cec59d7e

--- app/common/directives/icon-provider.js
+++ app/common/directives/icon-provider.js
@@ -8,21 +8,23 @@
  * Example: <icon file="icon-export.svg"></icon>
  *
  */
-window.angular && ((angular) => {
-  'use-strict';
+window.angular &&
+  (angular => {
+    'use-strict';
 
-  angular.module('app.common.directives').directive('icon', () => {
-    return {
-      restrict: 'E', link: (scope, element, attrs) => {
-        const file = attrs.file;
-        if (file === undefined) {
-          console.log('File name not provided for <icon> directive.')
-          return;
-        }
-        const svg = require(`../../assets/icons/${file}`);
-        element.html(svg);
-        element.addClass('icon');
-      }
-    }
-  })
-})(window.angular);
+    angular.module('app.common.directives').directive('icon', () => {
+      return {
+        restrict: 'E',
+        link: (scope, element, attrs) => {
+          const file = attrs.file;
+          if (file === undefined) {
+            console.log('File name not provided for <icon> directive.');
+            return;
+          }
+          const svg = require(`../../assets/icons/${file}`);
+          element.html(svg);
+          element.addClass('icon');
+        },
+      };
+    });
+  })(window.angular);
