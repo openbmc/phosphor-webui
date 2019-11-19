5fcee5ab526d678c9f39763cc90f600c2869df5d
Dixsie Wolmers <dixsiew@gmail.com>
2019-11-19 15:24:00 -0600

    Add file upload component
    Created reusuable file upload component to be used on updated firmware page.
    Component can:
    -Upload file
    -Display status icon
    -Clear upload field
    
    Made minor style change to file upload on certificate management.
    
    Tested in GUI.
    
    Signed-off-by: Dixsie Wolmers <dixsiew@gmail.com>
    Change-Id: I09bf56eee4d670681ea5e95c1807f8177c0e4c08

--- app/index.js
+++ app/index.js
@@ -73,6 +73,7 @@ import table_toolbar_component from './common/components/table/table-toolbar.js'
 import table_checkbox from './common/components/table/table-checkbox.js';
 import status_icon from './common/components/status-icon.js';
 import alert_banner from './common/components/alert-banner.js';
+import file_upload from './common/components/file-upload.js';
 
 import login_index from './login/index.js';
 import login_controller from './login/controllers/login-controller.js';
