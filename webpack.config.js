d87135943df315a2d376dda93afe22ccf7809b4c
Ed Tanous <ed@tanous.net>
2020-07-16 07:33:07 -0700

    Drop encoding library
    Commit f4a43cca440535a63592877c9bdbbe28636d803c attempted to fix a IE
    bug in the SOL library.  Unfortunately, it did it by pulling in the
    entire NPM encoding library, which is insanely large (190kb after
    minification and compression).  This is almost equivalent to the rest of
    our javascript put together!  That's nuts, and I don't think anyone
    would argue that's a binary hit worth taking for IE9 support.
    
    This commit removes it, and swaps in an inline polyfill from the mozilla
    recommendations, that compresses much....much smaller.
    
    Before this patchset build prints:
    app.bundle.js.gz    522 KiB          [emitted]  [big]
    
    After this patchset:
    app.bundle.js.gz    332 KiB          [emitted]  [big]
    
    If you don't want to break out a calculator, that's 190KB savings in the
    root filesystem, and should cut the initial page load time (on slow
    connections) by nearly 30 percent.
    
    Note: text-encodings was never pinned in the package-lock.json, hence
    why you don't see a diff removing it from package lock.  This was a miss
    on the original commiters part, although it doesn't really matter, as
    it's now removed.
    
    PS: A reviewer may note, I could've moved the polyfill into its own
    file.  Considering the serial console is the only page that uses that,
    and it's not that big I elected not to, to make removing it analagous
    with removing xterm/the serial console.
    
    Bonus content!
    Added a (commented out) webpack config that allows you to generate the
    webui as individual gzipped fragments, instead of one bundle.  This
    allows you to see file sizes on a package by package basis, and was how
    I found this commit in question.
    
    Testing:
    This is where this patchset gets hairy.  I dont' have an IE9 instance to
    test on.  Given it's low usage, and the fact that it's a relatively
    straightforward change we can probably just wait for someone to tell us
    it's broken again.
    
    Enjoy the sweet sweet savings.
    
    Signed-off-by: Ed Tanous <ed@tanous.net>
    Change-Id: I2820ff1c4b33d725ebc63490793a72fe600b8ed3
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- webpack.config.js
+++ webpack.config.js
@@ -142,6 +142,33 @@ module.exports = (env, options) => {
     })
   ];
 
+  // Comment in to see per-module js sizes.  This is useful in debugging "why is
+  // my binary so big"
+  /*
+  config.optimization = {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName =
+  module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @
+  symbols return `${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  };
+  */
+
   // Add build specific plugins
   if (isProd) {
     config.plugins.push(new CompressionPlugin({deleteOriginalAssets: true}));
