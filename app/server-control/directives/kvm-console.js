d77e53c1cf39547a8b755940c38d93d809d520ce
Tejas Patil <tejaspp@ami.com>
2021-09-23 13:03:27 +0530

    KVM: Add support for User Defined Macro Keys
    This commit add support for the User Defined Macro
    keys feature in KVM page.
    
    This enables 10 user defined key combination macros,
    which user can send to the host machine via KVM.
    
    This helps user to perform some different tasks with
    host, which required shortcut keys.
    
    Testing:
    Tested all the below listed macro, working as expected.
    
    'Alt+Tab' - Switch the between different opened windows
    'Ctrl+Alt+Del' - Send the Ctrl+Alt+Del key sequence
    'Alt+Space' - Opens menu option for the active program
    'Alt+Esc' - Switch application instantly with different
    'Alt+Enter' - Opens properties for selected item
    'Print' - Takes screenshot of active program and save it
    'Alt+F4' - closes the active window
    'Ctrl+Tab' - switch between opened tabs in browser or terminal
    'Super+L' - Lock the logged session
    
    Signed-off-by: Tejas Patil <tejaspp@ami.com>
    Change-Id: Ie9120ed9d3d82f15ef4f6c1504e357689bfa88a5

--- app/server-control/directives/kvm-console.js
+++ app/server-control/directives/kvm-console.js
@@ -6,6 +6,7 @@
  * @name kvmConsole
  */
 
+import KEY from '@novnc/novnc/core/input/keysym.js';
 import RFB from '@novnc/novnc/core/rfb.js';
 
 window.angular && (function(angular) {
@@ -29,6 +30,92 @@ window.angular && (function(angular) {
                 rfb.sendCtrlAltDel();
                 return false;
               };
+
+              scope.showMacroDropdown = false;
+              scope.macroList = [
+                'Alt+Tab',       // Switch between the different windows opened
+                'Ctrl+Alt+Del',  // Send the Ctrl+Alt+Del key sequence
+                'Alt+Space',     // Opens menu option of program
+                'Alt+Esc',       // Switch application instantly to different
+                'Alt+Enter',     // Opens properties for selected item
+                'Print',   // Takes the screen-shot of active window and save it
+                'Alt+F4',  // closes opened active window
+                'Ctrl+Esc',  // Opens start menu in windows os
+                'Ctrl+Tab',  // switch between opened tabs in in browser or
+                             // terminal
+                'Super+L'
+              ];  // log out
+
+              scope.sendMacro =
+                  function(macro) {
+                scope.showMacroDropdown = false;
+                switch (macro) {
+                  case 'Alt+Tab':
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', true);
+                    rfb.sendKey(KEY.XK_Tab, 'Tab', true);
+                    rfb.sendKey(KEY.XK_Tab, 'Tab', false);
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', false);
+                    break;
+
+                  case 'Ctrl+Alt+Del':
+                    sendCtrlAltDel();
+                    break;
+                  case 'Alt+Space':
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', true);
+                    rfb.sendKey(KEY.XK_space, 'Space', true);
+                    rfb.sendKey(KEY.XK_space, 'Space', false);
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', false);
+                    break;
+
+                  case 'Alt+Esc':
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', true);
+                    rfb.sendKey(KEY.XK_Escape, 'Escape', true);
+                    rfb.sendKey(KEY.XK_Escape, 'Escape', false);
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', false);
+                    break;
+
+                  case 'Alt+Enter':
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', true);
+                    rfb.sendKey(KEY.XK_Return, 'Return', true);
+                    rfb.sendKey(KEY.XK_Return, 'Return', false);
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', false);
+                    break;
+
+                  case 'Print':
+                    rfb.sendKey(KEY.XK_Print, 'Print', true);
+                    rfb.sendKey(KEY.XK_Print, 'Print', false);
+                    break;
+
+                  case 'Alt+F4':
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', true);
+                    rfb.sendKey(KEY.XK_F4, 'F4', true);
+                    rfb.sendKey(KEY.XK_F4, 'F4', false);
+                    rfb.sendKey(KEY.XK_Alt_L, 'AltLeft', false);
+                    break;
+
+                  case 'Ctrl+Esc':
+                    rfb.sendKey(KEY.XK_Control_L, 'ControlLeft', true);
+                    rfb.sendKey(KEY.XK_Escape, 'Escape', true);
+                    rfb.sendKey(KEY.XK_Escape, 'Escape', false);
+                    rfb.sendKey(KEY.XK_Control_L, 'ControlLeft', false);
+                    break;
+
+                  case 'Ctrl+Tab':
+                    rfb.sendKey(KEY.XK_Control_L, 'ControlLeft', true);
+                    rfb.sendKey(KEY.XK_Tab, 'Tab', true);
+                    rfb.sendKey(KEY.XK_Tab, 'Tab', false);
+                    rfb.sendKey(KEY.XK_Control_L, 'ControlLeft', false);
+                    break;
+                  case 'Super+L':
+                    rfb.sendKey(KEY.XK_Super_L, 'SuperLeft', true);
+                    rfb.sendKey(KEY.XK_L, 'L', true);
+                    rfb.sendKey(KEY.XK_L, 'L', false);
+                    rfb.sendKey(KEY.XK_Super_L, 'SuperLeft', false);
+                    break;
+                  default:
+                    console.log('No Macro Pressed');
+                }
+              }
 
               function connected(e) {
                 $log.debug('RFB Connected');
